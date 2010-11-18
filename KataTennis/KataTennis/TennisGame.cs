f7e4d0672a37b255c4aea795ab89d9f5401aedd3
ilkerde <ilker.cetinkaya@gmbsg.com>
2010-11-18 23:08:21 +0100

    green: love love is not over.

---
+++ KataTennis/KataTennis/TennisGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataTennis
+{
+    public class TennisGame
+    {
+        public TennisGame(string score)
+        {
+        }
+
+        public bool IsOver { get; set; }
+    }
+}
---
+++ KataTennis/KataTennis/TennisGameTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace KataTennis
+{
+    public class TennisGameTests
+    {
+        [Fact]
+        public void When_Love_Love_Then_Game_Is_Not_Over()
+        {
+            TennisGame game = new TennisGame("0:0");
+            Assert.False(game.IsOver);
+        }
+    }
+}

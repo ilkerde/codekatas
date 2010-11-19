4e31bf8e57246299ccaed2491a8219826553c485
ilkerde <ilker.cetinkaya@gmbsg.com>
2010-11-19 21:26:40 +0100

    red: thiry forty + score p1 -> score: 40:40

--- KataTennis/KataTennis/TennisScoreTests.cs
+++ KataTennis/KataTennis/TennisScoreTests.cs
@@ -13,5 +13,15 @@ namespace KataTennis
             Assert.Equal(Score.Love, game.GetScoreOf(Player.One));
             Assert.Equal(Score.Love, game.GetScoreOf(Player.Two));
         }
+
+        [Fact]
+        public void When_Thirty_Forty_And_PlayerOne_Scores_Then_Score_Is_Forty_Forty()
+        {
+            TennisGame game = new TennisGame(Score.Thirty, Score.Forty);
+            game.GrantScoreTo(Player.One);
+
+            Assert.Equal(Score.Forty, game.GetScoreOf(Player.One));
+            Assert.Equal(Score.Forty, game.GetScoreOf(Player.Two));
+        }
     }
 }

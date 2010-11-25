23c3f7be59137cb647f396e71797707a0b5c058d
ilkerde <ilker.cetinkaya@gmbsg.com>
2010-11-25 12:25:40 +0100

    red: range must return translations

--- KataFizzBuzz/KataFizzBuzz/FizzBuzzTranslateRangeTests.cs
+++ KataFizzBuzz/KataFizzBuzz/FizzBuzzTranslateRangeTests.cs
@@ -17,5 +17,15 @@ namespace KataFizzBuzz
 
             Assert.That(translations, Has.Count.EqualTo(100));
         }
+
+        [Test]
+        public void Returns_Elements_Whose_Values_Match_The_Translation_Of_Their_Corresponding_Keys()
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            Dictionary<int, string> translations = fizzbuzz.TranslateRange(1, 15);
+
+            foreach (var translation in translations)
+                Assert.That(translation.Value, Is.EqualTo(fizzbuzz.Translate(translation.Key)));
+        }
     }
 }

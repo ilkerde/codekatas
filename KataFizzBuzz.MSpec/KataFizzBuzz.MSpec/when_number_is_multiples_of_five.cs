28fa730adf699a94f6e9f6f0194a66ed84513b8e
ilkerde <ilker@ilker.de>
2011-02-19 15:54:30 +0100

    -> multiples of five: red

---
+++ KataFizzBuzz.MSpec/KataFizzBuzz.MSpec/when_number_is_multiples_of_five.cs
@@ -0,0 +1,23 @@
+﻿using Machine.Specifications;
+
+namespace KataFizzBuzz.MSpec
+{
+    [Subject(typeof(FizzBuzz))]
+    public class when_number_is_multiples_of_five
+    {
+        It should_translate_to_Buzz
+            = () => _translation.ShouldEqual("Buzz");
+
+        Because of
+            = () => _translation = _fizzBuzz.Translate(10);
+
+        Establish context
+            = () =>
+              {
+                  _fizzBuzz = new FizzBuzz();
+              };
+
+        static FizzBuzz _fizzBuzz;
+        static string _translation;
+    }
+}

eeaff09c0fa3598e0eed3a31aa1d5b219c27ee3c
ilkerde <ilker@ilker.de>
2011-02-19 16:00:56 +0100

    -> no multiples of three or five: red

---
+++ KataFizzBuzz.MSpec/KataFizzBuzz.MSpec/when_number_is_no_multiples_of_three_or_five.cs
@@ -0,0 +1,23 @@
+﻿using Machine.Specifications;
+
+namespace KataFizzBuzz.MSpec
+{
+    [Subject(typeof(FizzBuzz))]
+    public class when_number_is_no_multiples_of_three_or_five
+    {
+        It should_translate_to_number
+            = () => _translation.ShouldEqual("7");
+
+        Because of
+            = () => _translation = _fizzBuzz.Translate(7);
+
+        Establish context
+            = () =>
+              {
+                  _fizzBuzz = new FizzBuzz();
+              };
+
+        static FizzBuzz _fizzBuzz;
+        static string _translation;
+    }
+}

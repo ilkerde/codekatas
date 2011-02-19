cae94c555d8a2c73356ff47f2f180f99be2458e0
ilkerde <ilker@ilker.de>
2011-02-19 15:57:26 +0100

    -> multiples of three and five: red

---
+++ KataFizzBuzz.MSpec/KataFizzBuzz.MSpec/when_number_is_multiples_of_three_and_five.cs
@@ -0,0 +1,23 @@
+﻿using Machine.Specifications;
+
+namespace KataFizzBuzz.MSpec
+{
+    [Subject(typeof(FizzBuzz))]
+    public class when_number_is_multiples_of_three_and_five
+    {
+        It should_translate_to_FizzBuzz
+            = () => _translation.ShouldEqual("FizzBuzz");
+
+        Because of
+            = () => _translation = _fizzBuzz.Translate(15);
+
+        Establish context
+            = () =>
+              {
+                  _fizzBuzz = new FizzBuzz();
+              };
+
+        static FizzBuzz _fizzBuzz;
+        static string _translation;
+    }
+}

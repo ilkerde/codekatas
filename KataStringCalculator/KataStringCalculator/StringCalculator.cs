d4399f5d38dcbcfc57f66b71c80c89c04bf90ae2
ilkerde <ilker@ilker.de>
2011-02-22 20:58:21 +0100

    -> empty return zero : red

---
+++ KataStringCalculator/KataStringCalculator/StringCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataStringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KataStringCalculator/KataStringCalculator/StringCalculatorSpecs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Machine.Specifications;
+
+namespace KataStringCalculator
+{
+    public class StringCalculatorSpecs
+    {
+        public class when_source_is_empty : for_calculator_add
+        {
+            It should_result_to_zero
+                = () => result.ShouldEqual(0);
+
+            Because of
+                = () => result = calculator.Add(String.Empty);
+        }
+
+        public abstract class for_calculator_add
+        {
+            Establish context
+                = () => calculator = new StringCalculator();
+
+            protected static int result;
+            protected static StringCalculator calculator { get; private set; }
+        }
+    }
+}

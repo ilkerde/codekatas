586fae496b18ea640d2ed4b216efb6aa7e0c566b
ilkerde <ilker@ilker.de>
2011-03-25 21:20:50 +0100

    multiples of five - red

--- KataFizzBuzz.RSpec/fizzbuzz_spec.rb
+++ KataFizzBuzz.RSpec/fizzbuzz_spec.rb
@@ -4,5 +4,9 @@ describe "fizzbuzz, translate" do
 	it "returns fizz for multiples of three" do
 		FizzBuzz.new.translate(3).should == "fizz"
 	end
+
+	it "returns buzz for multiples of five" do
+		FizzBuzz.new.translate(5).should == "buzz"
+	end
 end
 

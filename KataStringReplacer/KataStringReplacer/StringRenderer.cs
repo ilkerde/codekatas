ff0b71e482901a13ab99b7310c8b9e99bd78f182
ilkerde <ilker@ilker.de>
2011-04-04 19:22:37 +0200

    template with placeholder -> string with value : green

--- KataStringReplacer/KataStringReplacer/StringRenderer.cs
+++ KataStringReplacer/KataStringReplacer/StringRenderer.cs
@@ -1,15 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KataStringReplacer
 {
 	public class StringRenderer
 	{
-		public StringRenderer()
-		{
+		private Dictionary<string, string> _placeholders;
+		
+		public StringRenderer(Dictionary<string, string> placeholders)
+		{ 
+			_placeholders = placeholders;
 		}
 		
 		public string Render(string template)
 		{
+			if (_placeholders != null)
+				foreach (var keyValue in _placeholders)
+					template = template.Replace(String.Format("${0}$", keyValue.Key), keyValue.Value);
+				
 			return template;
 		}
 	}

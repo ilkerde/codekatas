b9c3e9a6131d0c6a9c77ca990aab948779066502
ilkerde <ilker@ilker.de>
2011-04-04 17:31:31 +0200

    plain text template -> plain text string : red

--- KataStringReplacer/KataStringReplacer/StringRendererTests.cs
+++ KataStringReplacer/KataStringReplacer/StringRendererTests.cs
@@ -13,5 +13,15 @@ namespace KataStringReplacer {
 			
 			Assert.That(text, Is.Empty);
 		}
+		
+		[Test]
+		public void When_Template_Contains_Text_Without_Placeholders_Then_Text_Is_Same_As_Template() {
+			StringRenderer renderer = new StringRenderer();
+			string template = "A simple text message.";
+			
+			string text = renderer.Render(template);
+			
+			Assert.That(text, Is.EqualTo(template));
+		}
 	}
 }

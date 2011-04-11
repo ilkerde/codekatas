89cd7b0bc00964a6835f388dae6e46c1c8fc7221
ilkerde <ilker@ilker.de>
2011-04-11 23:38:23 +0200

    range translates entries : green

--- KataFizzBuzz.Java/src/FizzBuzzRange.java
+++ KataFizzBuzz.Java/src/FizzBuzzRange.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzzRange {
+	private FizzBuzz _fizzbuzz;
+	
 	public FizzBuzzRange(FizzBuzz fizzbuzz) {
-		// TODO Auto-generated constructor stub
+		_fizzbuzz = fizzbuzz;
 	}
 
 	public List<String> Range(int from, int to) {
 		List<String> translations = new ArrayList<String>();
 		
 		for (int i=from; i<=to; i++)
-			translations.add(null);
+			translations.add(_fizzbuzz.Translate(i));
 		
 		return translations;
 	}

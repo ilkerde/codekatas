1bdba81cc35c6830eff47e6a17b095e31b47e9b7
ilkerde <ilker@ilker.de>
2011-04-11 22:40:55 +0200

    multiples of three -> fizz : red

--- KataFizzBuzz.Java/src/FizzBuzz.java
+++ KataFizzBuzz.Java/src/FizzBuzz.java
@@ -1,2 +1,7 @@
 public class FizzBuzz {
+	public String Translate(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
+
--- KataFizzBuzz.Java/src/FizzBuzzTests.java
+++ KataFizzBuzz.Java/src/FizzBuzzTests.java
@@ -1,2 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class FizzBuzzTests {
+	@Test
+	public void multiples_of_three_translates_to_fizz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String translation = fizzbuzz.Translate(6);
+		assertEquals("fizz", translation);
+	}
 }

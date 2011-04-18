713c29ab49e978c0a8bf6eb9e3d43298fd02fafd
ilkerde <ilker@ilker.de>
2011-04-18 22:26:06 +0200

    akin left+right -> left empty : red

--- KataPickAkin/src/PickAkinTests.cs
+++ KataPickAkin/src/PickAkinTests.cs
@@ -9,18 +9,30 @@ namespace KataPickAkin
 		[TestFixture]
 		public class When_Left_Has_One_Akin_Of_One_Right
 		{
+			List<string> left = new List<string>() { "A1" };
+			List<string> right = new List<string>() { "A2" };
+
+			Board board = null;
+			
+			[SetUp]
+			public void Because()
+			{
+				board = left.PickAkin(right);
+			}
+			
 			[Test]
 			public void Then_Akin_Has_Both()
 			{
-				List<string> left = new List<string>() { "A1" };
-				List<string> right = new List<string>() { "A2" };
-		
-				Board board = left.PickAkin(right);
 			
 				Assert.That(board.Akin, Contains.Item("A1"));
 				Assert.That(board.Akin, Contains.Item("A2"));
 			}
+			
+			[Test]
+			public void Then_Left_Is_Empty()
+			{
+				Assert.That(board.Left, Is.Empty);
+			}
 		}
 	}
 }
-

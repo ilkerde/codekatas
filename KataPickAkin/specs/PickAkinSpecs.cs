2b27f287ffaaa3fa582d1a320df7a547bc0d15d7
ilkerde <ilker@ilker.de>
2011-04-19 18:20:52 +0200

    when multiples and mixes -> green

--- KataPickAkin/specs/PickAkinSpecs.cs
+++ KataPickAkin/specs/PickAkinSpecs.cs
@@ -32,6 +32,12 @@ namespace KataPickAkin {
 			It should_have_no_akin =
 				() => result.AkinList.ShouldBeEmpty();
 
+			It should_have_left_untouched =
+				() => result.LeftCodeList.ShouldContain("A1");
+
+			It should_have_right_untouched = 
+				() => result.RightCodeList.ShouldContain("B1");
+
 			Because of =
 				() => result = new PickAkin(left, right).Pick();
 
@@ -39,6 +45,29 @@ namespace KataPickAkin {
 				() => {
 					left = new List<string> { "A1" };
 					right = new List<string> { "B1" };
+				};
+
+			static List<string> left, right;
+			static PickAkinResult result;
+		}
+
+		public class when_left_contains_two_akin_and_one_disjunct_product_of_right {
+			It should_have_both_from_left_and_right_in_akin =
+				() => result.AkinList.ShouldContain(new [] {"A1", "A2", "A3"});
+
+			It should_have_one_disjunct_product_in_left =
+				() => result.LeftCodeList.ShouldContain("C1");
+
+			It should_have_one_disjunct_product_in_right =
+				() => result.RightCodeList.ShouldContain("B1");
+
+			Because of =
+				() => result = new PickAkin(left, right).Pick();
+
+			Establish context =
+				() => {
+					left = new List<string> { "A1", "A3", "C1" };
+					right = new List<string> { "A2", "B1" };
 				};
 
 			static List<string> left, right;

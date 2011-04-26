ca527b7c703624009eb22c7e458db1a3494b6643
ilkerde <ilker@ilker.de>
2011-04-26 11:10:05 +0200

    empty template -> empty text : red

---
+++ KataStringReplacer.XUnit/KataStringReplacer.XUnit/StringReplacer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KataStringReplacer.XUnit {
+    public class StringReplacer {
+        public string Replace(string template) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KataStringReplacer.XUnit/KataStringReplacer.XUnit/StringReplacerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace KataStringReplacer.XUnit
+{
+    public class StringReplacerTests {
+        [Fact]
+        public void when_template_is_emply_then_text_is_empty() {
+            var template = string.Empty;
+            var replacer = new StringReplacer();
+
+            var text = replacer.Replace(template);
+
+            Assert.Equal(string.Empty, text);
+        }
+    }
+}

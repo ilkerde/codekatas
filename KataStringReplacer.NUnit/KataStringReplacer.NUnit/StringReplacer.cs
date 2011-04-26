aea38a594015bdd49e75d66a247cc60f2452a932
ilkerde <ilker@ilker.de>
2011-04-26 07:57:50 +0200

    template empty -> text empty : red

---
+++ KataStringReplacer.NUnit/KataStringReplacer.NUnit/StringReplacer.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace KataStringReplacer.NUnit
+{
+    public class StringReplacer
+    {
+        public string Replace(string template)
+        {
+            throw new NotImplementedException();
+        }
+        public StringReplacer()
+        {
+
+        }
+    }
+}
---
+++ KataStringReplacer.NUnit/KataStringReplacer.NUnit/StringReplacerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace KataStringReplacer.NUnit
+{
+    [TestFixture]
+    public class StringReplacerTests
+    {
+        [Test]
+        public void when_template_is_empty_then_text_is_empty() 
+        {
+            StringReplacer replacer = new StringReplacer();
+            string text = replacer.Replace(string.Empty);
+
+            Assert.That(text, Is.Empty);
+        }
+    }
+}

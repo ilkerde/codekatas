5e0899da6f8f2454a50b62765c6675ba912991ea
ilkerde <ilker@ilker.de>
2011-04-26 11:20:21 +0200

    keyed template -> valued text : red

--- KataStringReplacer.XUnit/KataStringReplacer.XUnit/StringReplacer.cs
+++ KataStringReplacer.XUnit/KataStringReplacer.XUnit/StringReplacer.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KataStringReplacer.XUnit {
     public class StringReplacer {
+        private Dictionary<string, string> _table;
+
+        public StringReplacer(Dictionary<string, string> table)
+        {
+            _table = table;
+        }
+
         public string Replace(string template) {
             return template;
         }

3b61b3dbe2a7442e8305aa4b604d09451bce9a44
ilkerde <ilker@ilker.de>
2011-04-26 11:26:36 +0200

    keyed template -> valued text : green

--- KataStringReplacer.XUnit/KataStringReplacer.XUnit/StringReplacer.cs
+++ KataStringReplacer.XUnit/KataStringReplacer.XUnit/StringReplacer.cs
@@ -7,11 +7,17 @@ namespace KataStringReplacer.XUnit {
 
         public StringReplacer(Dictionary<string, string> table)
         {
-            _table = table;
+            _table = table ?? new Dictionary<string, string>();
         }
 
         public string Replace(string template) {
-            return template;
+            var text = template;
+
+            foreach (var entry in _table) {
+                text = text.Replace(String.Format("${0}$", entry.Key), entry.Value);
+            }
+
+            return text;
         }
     }
 }

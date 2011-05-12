5a9bbc15372c9ddd01987de27c2317d56f6cd321
ilkerde <ilker@ilker.de>
2011-05-12 15:44:16 +0200

    tidied up specs, no var needed

--- KataStringReplacer.Jasmine/spec/ReplacerSpecs.js
+++ KataStringReplacer.Jasmine/spec/ReplacerSpecs.js
@@ -1,12 +1,6 @@
 describe('replacing an empty template', function(){
-  var target;
-
-  beforeEach(function() {
-    target = Replacer;
-  });
-
   it('should yield to an empty text', function(){
-    expect(target.replace('')).toEqual('');
+    expect(Replacer.replace('')).toEqual('');
   });
 });
 

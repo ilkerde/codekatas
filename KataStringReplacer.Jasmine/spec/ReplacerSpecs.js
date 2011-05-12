c4d3cc73995a970dc7204ad42356b34777ac0ffb
ilkerde <ilker@ilker.de>
2011-05-12 15:42:26 +0200

    template with text only -> text : red

--- KataStringReplacer.Jasmine/spec/ReplacerSpecs.js
+++ KataStringReplacer.Jasmine/spec/ReplacerSpecs.js
@@ -9,3 +9,9 @@ describe('replacing an empty template', function(){
     expect(target.replace('')).toEqual('');
   });
 });
+
+describe('replacing a text without placeholders', function(){
+  it('should yield to given text', function(){
+    expect(Replacer.replace('text')).toEqual('text');
+  });
+});

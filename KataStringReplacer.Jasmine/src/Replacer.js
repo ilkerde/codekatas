ce1d01abe978863b849389fbe28ed7ac47246886
ilkerde <ilker@ilker.de>
2011-05-12 17:37:12 +0200

    template with key -> value : green

--- KataStringReplacer.Jasmine/src/Replacer.js
+++ KataStringReplacer.Jasmine/src/Replacer.js
@@ -1,5 +1,12 @@
 var Replacer = {
   replace: function(template) {
-    return template;
-  }
+    var text = template;
+    for (key in this.placeholders) {
+      text = text.replace("$" + key + "$", this.placeholders[key]);
+    }
+
+    return text;
+  },
+
+  placeholders: {}
 };

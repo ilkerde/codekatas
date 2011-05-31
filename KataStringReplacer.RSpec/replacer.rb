30c9e9f52472e23f50284f774a6357d462734a1b
Ilker Cetinkaya <ilker@ilker.de>
2011-05-31 09:00:59 +0200

    template with unkown placeholder -> text only : green

--- KataStringReplacer.RSpec/replacer.rb
+++ KataStringReplacer.RSpec/replacer.rb
@@ -3,5 +3,8 @@ def replacer template, placeholders={}
   placeholders.each do |key,value|
     text.gsub! "$#{key}$", value
   end
+  if (text =~ /\$.*?\$/)
+    text.gsub! /\$.*?\$/, ""
+  end
   text
 end

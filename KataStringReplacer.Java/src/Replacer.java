ac63aca138d045d699fa0ef1aab31a77793d9f8c
Ilker Cetinkaya <ilker@ilker.de>
2011-05-31 20:53:19 +0200

    extract placeholder token => blue

--- KataStringReplacer.Java/src/Replacer.java
+++ KataStringReplacer.Java/src/Replacer.java
@@ -9,7 +9,7 @@ public class Replacer {
 		
 		if (_placeholders != null)
 			for (Map.Entry<String, String> placeholder : _placeholders.entrySet())
-				text = text.replaceAll(String.format("\\$%s\\$", placeholder.getKey()), placeholder.getValue());
+				text = text.replaceAll(getPlaceholderToken(placeholder), placeholder.getValue());
 		
 		return text;
 	}
@@ -17,5 +17,9 @@ public class Replacer {
 	public static void setPlaceholders(HashMap<String, String> placeholders) {
 		Replacer._placeholders = new HashMap<String, String>();
 		Replacer._placeholders.putAll(placeholders);
+	}
+
+	private static String getPlaceholderToken(Map.Entry<String, String> placeholder) {
+		return String.format("\\$%s\\$", placeholder.getKey());
 	}
 }

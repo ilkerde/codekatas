deadf5dd92bd6805788073ec146e8266d9b9411e
Ilker Cetinkaya <ilker@ilker.de>
2011-05-31 20:50:20 +0200

    template with known key => text with value : green

--- KataStringReplacer.Java/src/Replacer.java
+++ KataStringReplacer.Java/src/Replacer.java
@@ -1,12 +1,21 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Replacer {
+	private static HashMap<String, String> _placeholders;
+	
 	public static String replace(String template) {
-		return template;
+		String text = template;
+		
+		if (_placeholders != null)
+			for (Map.Entry<String, String> placeholder : _placeholders.entrySet())
+				text = text.replaceAll(String.format("\\$%s\\$", placeholder.getKey()), placeholder.getValue());
+		
+		return text;
 	}
 
-	public static void setPlaceholders(HashMap<String, String> _placeholders) {
-		// TODO Auto-generated method stub
-		
+	public static void setPlaceholders(HashMap<String, String> placeholders) {
+		Replacer._placeholders = new HashMap<String, String>();
+		Replacer._placeholders.putAll(placeholders);
 	}
 }

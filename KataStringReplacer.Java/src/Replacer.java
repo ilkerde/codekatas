46a924549f5986b5afcd103fb1db5ecbf4dda392
Ilker Cetinkaya <ilker@ilker.de>
2011-05-31 21:02:26 +0200

    touchdown. polished up replacer static class. wow, this kata in java is so unbelievably different. java is a ceremony :P

--- KataStringReplacer.Java/src/Replacer.java
+++ KataStringReplacer.Java/src/Replacer.java
@@ -5,20 +5,29 @@ public class Replacer {
 	private static HashMap<String, String> _placeholders;
 	
 	public static String replace(String template) {
-		String text = template;
-		
-		if (_placeholders != null)
-			for (Map.Entry<String, String> placeholder : _placeholders.entrySet())
-				text = text.replaceAll(getPlaceholderTokenRegex(placeholder), placeholder.getValue());
-		
-		text = text.replaceAll("\\$.*?\\$", "");
-		
-		return text;
+		return
+		removeUnknownPlaceholderTokens(
+			replaceKnownPlaceholderTokens(
+				template
+			)
+		);
 	}
 
 	public static void setPlaceholders(HashMap<String, String> placeholders) {
 		Replacer._placeholders = new HashMap<String, String>();
 		Replacer._placeholders.putAll(placeholders);
+	}
+
+	private static String replaceKnownPlaceholderTokens(String text) {
+		if (_placeholders != null)
+			for (Map.Entry<String, String> placeholder : _placeholders.entrySet())
+				text = text.replaceAll(getPlaceholderTokenRegex(placeholder), placeholder.getValue());
+		return text;
+	}
+
+	private static String removeUnknownPlaceholderTokens(String text) {
+		text = text.replaceAll("\\$.*?\\$", "");
+		return text;
 	}
 
 	private static String getPlaceholderTokenRegex(Map.Entry<String, String> placeholder) {

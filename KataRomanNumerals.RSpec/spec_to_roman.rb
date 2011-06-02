963e380b1c7f5d6b7be9083415a8fce68bba3441
Ilker Cetinkaya <ilker@ilker.de>
2011-06-02 23:14:32 +0200

    5 -> V : red

--- KataRomanNumerals.RSpec/spec_to_roman.rb
+++ KataRomanNumerals.RSpec/spec_to_roman.rb
@@ -6,4 +6,10 @@ describe "to_roman" do
       1.to_roman.should == "I"
     end
   end
+
+  describe "when number is 5" do
+    it "should return V" do
+      5.to_roman.should == "V"
+    end
+  end
 end

68424a0c6a7ba96c00659fe3ad3b6cd832f2e934
Ilker Cetinkaya <ilker@ilker.de>
2011-06-02 23:16:34 +0200

    10 -> X : red

--- KataRomanNumerals.RSpec/spec_to_roman.rb
+++ KataRomanNumerals.RSpec/spec_to_roman.rb
@@ -12,4 +12,10 @@ describe "to_roman" do
       5.to_roman.should == "V"
     end
   end
+
+  describe "when number is 10" do
+    it "should return X" do
+      10.to_roman.should == "X"
+    end
+  end
 end

dac2b79102660b7fce0986231c0ea58520ba9bc2
ilkerde <ilker@ilker.de>
2011-06-03 17:04:16 +0200

    II -> II : red

--- KataRomanNumerals.NUnit/RomanNumbersTests.cs
+++ KataRomanNumerals.NUnit/RomanNumbersTests.cs
@@ -8,5 +8,10 @@ namespace RomanNumbers {
     public void when_number_is_1_then_converts_to_I() {
       Assert.That(1.ToRoman(), Is.EqualTo("I"));
     }
+
+    [Test]
+    public void when_number_is_2_then_converts_to_II() {
+      Assert.That(2.ToRoman(), Is.EqualTo("II"));
+    }
   }
 }

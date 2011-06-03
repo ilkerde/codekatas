a27f9ce36e3930882410874aa15e9c67b5d736c3
ilkerde <ilker@ilker.de>
2011-06-03 17:38:05 +0200

    5 -> V : green

--- KataRomanNumerals.NUnit/RomanNumbers.cs
+++ KataRomanNumerals.NUnit/RomanNumbers.cs
@@ -1,12 +1,28 @@
 namespace RomanNumbers {
   using System;
+  using System.Collections.Generic;
+  using System.Linq;
 
   public static class RomanNumberExtensions {
+    private class RomanValue {
+      public string Roman;
+      public int Value;
+    }
+
+    private static IEnumerable<RomanValue> romanvalues = 
+      (new[] { 5, 1 })
+        .Zip(new [] { "V", "I" },
+        (value, roman) => new RomanValue { Roman = roman, Value = value });
+
     public static string ToRoman(this int number) {
       string result = string.Empty;
 
-      while (number-- >= 1)
-        result += "I";
+      foreach (var romanvalue in romanvalues) {
+        while (number >= romanvalue.Value) {
+          result += romanvalue.Roman;
+          number -= romanvalue.Value;
+        }
+      }
 
       result = result.Replace("IIII", "IV");
 

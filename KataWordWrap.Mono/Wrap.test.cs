e3040485a14ef3ba650fb9df8d849c3ed2737fc5
Ilker Cetinkaya <ilker@ilker.de>
2011-06-24 22:23:02 +0200

    single word > marker -> wrap : red

--- KataWordWrap.Mono/Wrap.test.cs
+++ KataWordWrap.Mono/Wrap.test.cs
@@ -9,5 +9,11 @@ namespace KataWordWrap {
       var result = Wrapper.Wrap("word", 6);
       Assert.AreEqual("word", result);
     }
+
+    [Test]
+    public void When_Text_Is__Word__And_Column_Marker_Is_2_Then_Does_Wrap() {
+      var result = Wrapper.Wrap("word", 2);
+      Assert.AreEqual("wo\nrd", result);
+    }
   }
 }

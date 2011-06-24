213067780b58ca34d77ccba36771203f36d50ae8
ilkerde <ilker@ilker.de>
2011-06-24 15:48:44 +0200

    better door initialization : blue

--- Kata100Doors.NUnit/100Doors.cs
+++ Kata100Doors.NUnit/100Doors.cs
@@ -3,13 +3,21 @@ namespace HundredDoorsKata {
   using System.Linq;
 
   public class Floor {
-    private Door[] doors = Enumerable.Range(1, 100).Select(i => new Door{ IsClosed = true }).ToArray();
+    private Door[] doors = 
+      Enumerable.Range(1, 100)
+      .Select(i => new Door())
+      .ToArray();
+
     public Door[] GetDoors() {
       return doors; 
     }
   }
 
   public class Door {
+    public Door() {
+      IsClosed = true;
+    }
+
     public bool IsClosed { get; set; }
     public bool IsOpen { get; set; }
   }

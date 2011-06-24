1d83181b8cc6adf219de79f86abc5bb673265dd7
ilkerde <ilker@ilker.de>
2011-06-24 15:20:54 +0200

    Kata 100 Doors

---
+++ Kata100Doors.NUnit/rakefile.rb
@@ -0,0 +1,25 @@
+require 'fileutils'
+
+$ME = File.dirname __FILE__
+$HOME = ENV['HOME']
+
+source = File.join $ME, "*.cs"
+binary = File.join $ME, "bin/HundredDoors.dll"
+
+nunitlib = File.join $ME, "lib/nunit/framework/nunit.framework.dll"
+nunitrun = File.join $ME, "lib/nunit/nunit-console.exe"
+desc "clean"
+task :clean do
+    FileUtils.rm_f binary
+end
+
+desc "build"
+task :build => [:clean] do
+    FileUtils.cp nunitlib, File.join(File.dirname(binary), File.basename(nunitlib))
+    system "csc /t:library /out:#{binary} /r:#{nunitlib} /nologo /debug /recurse:#{source}"
+end
+
+desc "test"
+task :test => [:build] do
+    system "#{nunitrun} #{binary}"
+end

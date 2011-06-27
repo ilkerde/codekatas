9ace3003ab2a9a2f31e7c5a1fe0ef23f478b7517
ilkerde <ilker@ilker.de>
2011-06-27 18:11:30 +0200

    word > mark -> wrap : red

--- KataWordWrap.NUnit/TestWordWarp.cs
+++ KataWordWrap.NUnit/TestWordWarp.cs
@@ -9,5 +9,11 @@ namespace KataWordWrap {
       var result = Wrapper.Wrap("word", 7);
       Assert.AreEqual("word", result);
     }
+
+    [Test]
+    public void When_word_is_larger_than_wrap_marker_then_does_wrap() {
+      var result = Wrapper.Wrap("word", 2);
+      Assert.AreEqual("wo\nrd", result);
+    }
   }
 }

3bdeb7df173178c60e769bfa30d59f3ae78efdc5
ilkerde <ilker@ilker.de>
2011-06-27 18:27:57 +0200

    2 words > mark -> wrap : green

--- KataWordWrap.NUnit/WordWrap.cs
+++ KataWordWrap.NUnit/WordWrap.cs
@@ -3,8 +3,15 @@ namespace KataWordWrap {
 
   public class Wrapper {
     public static string Wrap(string text, int mark) {
-      if (text.Length > mark)
-        return text.Substring(0, mark) + "\n" + text.Substring(mark);
+      if (text.Length > mark) {
+        string firstLineCutoff = text.Substring(0, mark);
+        int lastSpacingIndex = firstLineCutoff.LastIndexOf(" ");
+
+        if (lastSpacingIndex < 0)
+          return text.Substring(0, mark) + "\n" + text.Substring(mark);
+
+        return text.Substring(0, lastSpacingIndex) + "\n" + text.Substring(lastSpacingIndex + 1);
+      }
 
       return text;
     }

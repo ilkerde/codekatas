a0b827798749f1b4c503a3c6b274064d2303ed71
ilkerde <ilker@ilker.de>
2011-06-28 13:28:06 +0200

    2 words + mark on 2nd => wrap on space : green

--- KataWordWrap.MSpec/WordWrap.cs
+++ KataWordWrap.MSpec/WordWrap.cs
@@ -6,7 +6,11 @@ namespace KataWordWrap {
       if (text.Length < size)
         return text;
 
-      return text.Substring(0, size) + '\n' + text.Substring(2);
+      int lastSpaceIndex = text.LastIndexOf(" ", size);
+      if (lastSpaceIndex < 0)
+        return text.Substring(0, size) + '\n' + text.Substring(2);
+
+      return text.Substring(0, lastSpaceIndex) + '\n' + text.Substring(lastSpaceIndex + 1);
     }
   }
 }

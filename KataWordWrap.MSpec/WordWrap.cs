9ca1a425bad137e15cf8863c369a1f1de6790a6b
ilkerde <ilker@ilker.de>
2011-06-28 14:27:28 +0200

    3 words + mark on 2nd => wrap twice on space : green

--- KataWordWrap.MSpec/WordWrap.cs
+++ KataWordWrap.MSpec/WordWrap.cs
@@ -3,21 +3,21 @@ namespace KataWordWrap {
 
   public class Wrapper {
     public static string Wrap(string text, int size) {
-      if (text.Length < size)
+      if (text.Length <= size)
         return text;
 
       int lastSpaceIndex = text.LastIndexOf(" ", size);
       if (lastSpaceIndex < 0)
-        return SplitText(text, size, false);
+        return SplitText(text, size, size, false);
 
-      return SplitText(text, lastSpaceIndex, true);
+      return SplitText(text, lastSpaceIndex, size, true);
     }
 
-    private static string SplitText(string text, int position, bool omitSpace) {
+    private static string SplitText(string text, int position, int size, bool omitSpace) {
       return 
           text.Substring(0, position) 
         + NewLine 
-        + text.Substring(position + (omitSpace ? 1 : 0));
+        + Wrap(text.Substring(position + (omitSpace ? 1 : 0)), size);
     }
 
     private const char NewLine = '\n';

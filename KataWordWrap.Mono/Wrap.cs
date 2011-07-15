ef896d809b0202852bbb7d83ab5eeb3cf9f1b605
Ilker Cetinkaya <ilker@ilker.de>
2011-07-15 22:16:39 +0200

    2 words marker on 2nd -> wrap at space : green

--- KataWordWrap.Mono/Wrap.cs
+++ KataWordWrap.Mono/Wrap.cs
@@ -6,6 +6,14 @@ namespace KataWordWrap {
       if (input.Length < columnmarker)
         return input;
 
+      string virtualLine = input.Substring(0, columnmarker);
+      int lastSpaceIndex = virtualLine.LastIndexOf(" ");
+
+      if (lastSpaceIndex > 0)
+        return input.Substring(0, lastSpaceIndex)
+          + "\n"
+          + input.Substring(lastSpaceIndex + 1);
+
       return input.Substring(0, columnmarker)
         + "\n"
         + input.Substring(columnmarker);

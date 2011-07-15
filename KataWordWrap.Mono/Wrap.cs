0e1a9dab6d2487c04aed73316d0b413db6c42792
Ilker Cetinkaya <ilker@ilker.de>
2011-07-15 22:42:40 +0200

    3 words, marker exceeds space -> wrap at space twice : green

--- KataWordWrap.Mono/Wrap.cs
+++ KataWordWrap.Mono/Wrap.cs
@@ -3,26 +3,26 @@ namespace KataWordWrap {
   
   public class Wrapper {
     public static string Wrap(string input, int columnmarker) {
-      if (input.Length < columnmarker)
+      if (input.Length <= columnmarker)
         return input;
 
       string virtualLine = input.Substring(0, columnmarker);
       int lastSpaceIndex = virtualLine.LastIndexOf(" ");
 
       if (lastSpaceIndex > 0)
-        return SplitLine(input, lastSpaceIndex, 1);
+        return SplitLine(input, lastSpaceIndex, columnmarker, 1);
 
       bool isMarkerBeforeSpace = 
         input.Length > columnmarker && 
         input[columnmarker] == ' ';
 
-      return SplitLine(input, columnmarker, isMarkerBeforeSpace ? 1 : 0);
+      return SplitLine(input, columnmarker, columnmarker, isMarkerBeforeSpace ? 1 : 0);
     }
 
-    private static string SplitLine(string text, int splitColumn, int omitCharsAtSplit=0) {
+    private static string SplitLine(string text, int splitColumn, int columnMarker, int omitCharsAtSplit=0) {
       return text.Substring(0, splitColumn)
         + "\n"
-        + text.Substring(splitColumn + omitCharsAtSplit);
+        + Wrap(text.Substring(splitColumn + omitCharsAtSplit), columnMarker);
     }
   }
 }

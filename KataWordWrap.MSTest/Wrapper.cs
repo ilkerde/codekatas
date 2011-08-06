4ebdd059d3a8c25fe9a614401e35abcd78e342f8
Ilker Cetinkaya <ilker@ilker.de>
2011-08-06 21:53:27 +0200

    touchdown. the last test of this kata is always a refreshing thing. mstest feels odd and is kind of slow for me. cleaned up implementation a little : blue

--- KataWordWrap.MSTest/Wrapper.cs
+++ KataWordWrap.MSTest/Wrapper.cs
@@ -2,14 +2,17 @@
 {
   public static class Wrapper {
     public static string Wrap(string text, int marker) {
+      const string newline = "\n";
+
       if (text.Length <= marker) return text;
+
       string line = text.Substring(0, marker + 1);
       int lastSpaceOnLine = line.LastIndexOf(" ");
 
       if(lastSpaceOnLine > 0)
-        return text.Substring(0, lastSpaceOnLine) + "\n" + Wrap(text.Substring(lastSpaceOnLine + 1), marker);
+        return text.Substring(0, lastSpaceOnLine) + newline + Wrap(text.Substring(lastSpaceOnLine + 1), marker);
 
-      return text.Substring(0, marker) + "\n" + Wrap(text.Substring(marker), marker);
+      return text.Substring(0, marker) + newline + Wrap(text.Substring(marker), marker);
     }
   }
 }

b8cddb0477a52d9d6e8805cd6a66f90a621159c1
Ilker Cetinkaya <ilker@ilker.de>
2011-08-06 15:22:14 +0200

    word > marker => wrap @ marker : green

--- KataWordWrap.MSTest/WrapTests.cs
+++ KataWordWrap.MSTest/WrapTests.cs
@@ -12,7 +12,7 @@ namespace KataWordWrap.MSTest {
 
   public static class Wrapper {
     public static string Wrap(string text, int marker) {
-      throw new NotImplementedException();
+      return text.Substring(0, marker) + "\n" + text.Substring(marker);
     }
   }
 }

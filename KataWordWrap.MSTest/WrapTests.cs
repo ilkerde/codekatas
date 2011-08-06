be8c90a1987231b1f6845f7a90dcd6a136d68301
Ilker Cetinkaya <ilker@ilker.de>
2011-08-06 15:30:49 +0200

    marker @ 2nd word => wrap @ space : red

--- KataWordWrap.MSTest/WrapTests.cs
+++ KataWordWrap.MSTest/WrapTests.cs
@@ -12,5 +12,9 @@ namespace KataWordWrap.MSTest {
     public void When_Word_Is_Shorter_Than_Marker_Then_Does_Not_Wrap() {
       Assert.AreEqual("wordwrap", Wrapper.Wrap("wordwrap", 12));
     }
+    [TestMethod]
+    public void When_Marker_Is_On_2nd_Word_Then_Wraps_At_Spacing() {
+      Assert.AreEqual("word\nwrap", Wrapper.Wrap("word wrap", 7));
+    }
   }
 }

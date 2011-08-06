fcf35db000b13e21287630dd8d44e72b0ac31c72
Ilker Cetinkaya <ilker@ilker.de>
2011-08-06 21:35:51 +0200

    marker @ 2nd word on sequence of 3 words => wrap @ space twice : green

--- KataWordWrap.MSTest/Wrapper.cs
+++ KataWordWrap.MSTest/Wrapper.cs
@@ -2,14 +2,14 @@
 {
   public static class Wrapper {
     public static string Wrap(string text, int marker) {
-      if (text.Length < marker) return text;
+      if (text.Length <= marker) return text;
       string line = text.Substring(0, marker + 1);
       int lastSpaceOnLine = line.LastIndexOf(" ");
 
       if(lastSpaceOnLine > 0)
-        return text.Substring(0, lastSpaceOnLine) + "\n" + text.Substring(lastSpaceOnLine + 1);
+        return text.Substring(0, lastSpaceOnLine) + "\n" + Wrap(text.Substring(lastSpaceOnLine + 1), marker);
 
-      return text.Substring(0, marker) + "\n" + text.Substring(marker);
+      return text.Substring(0, marker) + "\n" + Wrap(text.Substring(marker), marker);
     }
   }
 }

3c2fff700c06a11ff5a7bc95435fe4c324ffdf62
Ilker Cetinkaya <ilker@ilker.de>
2011-08-06 15:34:04 +0200

    marker @ 2nd word => wrap @ space : green

--- KataWordWrap.MSTest/Wrapper.cs
+++ KataWordWrap.MSTest/Wrapper.cs
@@ -3,6 +3,12 @@
   public static class Wrapper {
     public static string Wrap(string text, int marker) {
       if (text.Length < marker) return text;
+      string line = text.Substring(0, marker);
+      int lastSpaceOnLine = line.LastIndexOf(" ");
+
+      if(lastSpaceOnLine > 0)
+        return text.Substring(0, lastSpaceOnLine) + "\n" + text.Substring(lastSpaceOnLine + 1);
+
       return text.Substring(0, marker) + "\n" + text.Substring(marker);
     }
   }

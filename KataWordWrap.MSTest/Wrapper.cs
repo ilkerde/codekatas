bb50c14e587e3ac167d45919767eb5335e443789
Ilker Cetinkaya <ilker@ilker.de>
2011-08-06 15:28:17 +0200

    word < marker => nowrap : green

--- KataWordWrap.MSTest/Wrapper.cs
+++ KataWordWrap.MSTest/Wrapper.cs
@@ -2,6 +2,7 @@
 {
   public static class Wrapper {
     public static string Wrap(string text, int marker) {
+      if (text.Length < marker) return text;
       return text.Substring(0, marker) + "\n" + text.Substring(marker);
     }
   }

12b883207a5a90908b5c10becd8f5f69203b1bf2
Ilker Cetinkaya <ilker@ilker.de>
2011-08-06 15:13:52 +0200

    Wordwrap with MSTest

---
+++ KataWordWrap.MSTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataWordWrap.MSTest {
+  [TestClass]
+  public class UnitTest1 {
+    [TestMethod]
+    public void TestMethod1() {
+    }
+  }
+}

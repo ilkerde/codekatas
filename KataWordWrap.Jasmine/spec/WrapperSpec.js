9ca485ba582de8100145e9518eeb1a237f0664b0
Ilker Cetinkaya <ilker@ilker.de>
2011-08-09 23:13:52 +0200

    word > marker -> wrap : red

--- KataWordWrap.Jasmine/spec/WrapperSpec.js
+++ KataWordWrap.Jasmine/spec/WrapperSpec.js
@@ -1,2 +1,7 @@
 describe("wrap", function(){
+  describe("a long word being longer than marker", function(){
+    it("should wrap at marker", function(){
+      expect(wrap("wordwrap", 4)).toEqual("word\nwrap");
+    });
+  });
 });

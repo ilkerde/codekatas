f395f543dc38da2ef6104b2ccaf58f3c0f5df6d9
Ilker Cetinkaya <ilker@ilker.de>
2011-08-09 23:30:52 +0200

    2 words, marker @ 2nd -> wrap @ space : green

--- KataWordWrap.Jasmine/public/Wrapper.js
+++ KataWordWrap.Jasmine/public/Wrapper.js
@@ -2,5 +2,8 @@ var wrap = function(text, marker){
   if (text.length < marker)
     return text;
 
-  return "word\nwrap";
+  var lastspace = text.lastIndexOf(' ', marker);
+  var wrapat = lastspace > 0 ? lastspace : marker;
+  
+  return text.substring(0, wrapat).trim() + '\n' + text.substring(wrapat).trim();
 };

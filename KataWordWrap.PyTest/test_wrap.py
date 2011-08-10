d0a53d442c84f9fa491c78f1b80a036f7ed8cf5f
Ilker Cetinkaya <ilker@ilker.de>
2011-08-10 23:08:57 +0200

    short < marker -> no wrap : red

--- KataWordWrap.PyTest/test_wrap.py
+++ KataWordWrap.PyTest/test_wrap.py
@@ -4,3 +4,7 @@ def test_wrap_when_marker_is_within_word():
   result = wrap_at("longword", 4)
   assert result == "long\nword"
 
+def test_nowrap_when_word_is_shorter_than_marker():
+  result = wrap_at("short", 8)
+  assert result == "short"
+

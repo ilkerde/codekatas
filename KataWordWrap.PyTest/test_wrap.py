c77d2bc57fe58da2b138747b9a41853833cddf0f
Ilker Cetinkaya <ilker@ilker.de>
2011-08-10 23:22:06 +0200

    2 words with marker on 2nd -> wrap at space : red

--- KataWordWrap.PyTest/test_wrap.py
+++ KataWordWrap.PyTest/test_wrap.py
@@ -8,3 +8,7 @@ def test_nowrap_when_word_is_shorter_than_marker():
   result = wrap_at("short", 8)
   assert result == "short"
 
+def test_wrap_at_space_when_2_words_with_marker_on_2nd():
+  result = wrap_at("first second", 8)
+  assert result == "first\nsecond"
+

455f7e11a271c464b44e1dfa76bc27c9bb17932d
Ilker Cetinkaya <ilker@ilker.de>
2011-08-10 23:41:46 +0200

    touchdown. this was a nice exploration exercise. pytest is pretty straight forward, the test discovery conventions are easy enough. pythons ternary operator is kind of cool, like the index ranges es well. nice! - 3 words with marker on 2nd -> wrap twice : green

--- KataWordWrap.PyTest/wrap.py
+++ KataWordWrap.PyTest/wrap.py
@@ -1,8 +1,8 @@
 def wrap_at(text, marker):
-  if len(text) < marker:
+  if len(text) <= marker:
     return text
 
   lastspace = text[:marker].rfind(' ')
   cutoff = lastspace if lastspace > 0 else marker
 
-  return text[:cutoff].strip() + '\n' + text[cutoff:].strip()
+  return text[:cutoff].strip() + '\n' + wrap_at(text[cutoff:].strip(), marker)

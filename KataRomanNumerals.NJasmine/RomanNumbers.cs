223f5bd1ed258afc81c7b526c5ae487b16f50d55
Ilker Cetinkaya <ilker@ilker.de>
2011-08-11 20:29:14 +0200

    5 -> V : green

--- KataRomanNumerals.NJasmine/RomanNumbers.cs
+++ KataRomanNumerals.NJasmine/RomanNumbers.cs
@@ -3,7 +3,7 @@ namespace KataRomanNumbers {
 
   public static class RomanNumberExtensions {
     public static string ToRoman(this int number) {
-      return "I";
+      return number == 5 ? "V" : "I";
     }
   }
 }

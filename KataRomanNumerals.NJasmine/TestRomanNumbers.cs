ead1460760a7db803582ac36277662aaaae2acdd
Ilker Cetinkaya <ilker@ilker.de>
2011-08-11 20:30:16 +0200

    10 -> X : red

--- KataRomanNumerals.NJasmine/TestRomanNumbers.cs
+++ KataRomanNumerals.NJasmine/TestRomanNumbers.cs
@@ -13,6 +13,10 @@ namespace KataRomanNumbers {
         it("should value 5 with V", () => {
           expect(() => 5.ToRoman() == "V");
         });
+
+        it("should value 10 with X", () => {
+          expect(() => 10.ToRoman() == "X");
+        });
       });
     }
   }

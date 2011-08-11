c65ddf34acba43ea7670a4bb95b89a53ee0052f3
Ilker Cetinkaya <ilker@ilker.de>
2011-08-11 20:57:58 +0200

    2 -> II : green

--- KataRomanNumerals.NJasmine/RomanNumbers.cs
+++ KataRomanNumerals.NJasmine/RomanNumbers.cs
@@ -1,21 +1,27 @@
 namespace KataRomanNumbers {
   using System;
+  using System.Linq;
   using System.Collections.Generic;
 
   public static class RomanNumberExtensions {
-    private static Dictionary<int, string> romanLetters = new Dictionary<int, string> {
-      { 1000, "M" },
-      { 500, "D" },
-      { 100, "C" },
-      { 50, "L" },
-      { 10, "X" },
-      { 5, "V" },
-      { 1, "I" }
+    private static SortedDictionary<int, char> romanLetters = new SortedDictionary<int, char> {
+      { 1000, 'M' },
+      { 500, 'D' },
+      { 100, 'C' },
+      { 50, 'L' },
+      { 10, 'X' },
+      { 5, 'V' },
+      { 1, 'I' }
     };
 
     public static string ToRoman(this int number) {
-      if (romanLetters.ContainsKey(number))
-        return romanLetters[number];
+      foreach (var romanLetter in romanLetters.Reverse()) {
+        if (romanLetter.Key == number)
+          return romanLetter.Value.ToString();
+
+        if (romanLetter.Key < number)
+          return new String(romanLetter.Value, number);
+      }
 
       return null;
     }

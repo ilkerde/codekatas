3b7d8a93962400ce4205d19d7f86c86ac52201c4
Ilker Cetinkaya <ilker@ilker.de>
2011-08-19 01:12:28 +0200

    100 -> C : green

--- KataRomanNumerals.PowerSpec/roman_number.ps1
+++ KataRomanNumerals.PowerSpec/roman_number.ps1
@@ -1,9 +1,10 @@
 function to_roman($number) {
   $letter = @{
-    1  = "I";
-    5  = "V";
-    10 = "X";
-    50 = "L";
+    1   = "I";
+    5   = "V";
+    10  = "X";
+    50  = "L";
+    100 = "C";
   }
 
   $letter[$number]

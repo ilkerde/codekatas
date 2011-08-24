beda0f245d80752729cb41d05f4f5cf791fdcaaa
Ilker Cetinkaya <ilker@ilker.de>
2011-08-24 23:45:13 +0200

    20 -> XX : green

--- KataRomanNumerals.PowerSpec/roman_number.ps1
+++ KataRomanNumerals.PowerSpec/roman_number.ps1
@@ -12,9 +12,15 @@ function to_roman($number) {
   $roman = "";
 
   while ($number -gt 0) {
-    $letter.getenumerator() | sort -p "key" -desc | 
+    $letter.getenumerator()       | 
+    sort -p "key" -desc           | 
     ?{ ($number / $_.key) -ge 1 } | 
-    %{ $number -= $_.key; $roman += $_.value; }
+    select -first 1               |
+    %{ 
+      $times = $number % $_.key;
+      $number -= $_.key; 
+      $roman += $_.value; 
+    }
   }
 
   $roman = $roman.replace("IIII", "IV")

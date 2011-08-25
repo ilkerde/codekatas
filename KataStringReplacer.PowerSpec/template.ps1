a8fd3522080fb7a32d0a45b231fd0c8d340273e5
Ilker Cetinkaya <ilker@ilker.de>
2011-08-25 23:44:00 +0200

    touchdown. nice little templating with posh and powerspec. should asserts look awesome. ntl, i'm missing the 'describe' part for the sake of easy reading and comprehension. nb tried my slight modifications on powerspec for pscx compatibility. blue: using posh builtin replace for a better read ;)

--- KataStringReplacer.PowerSpec/template.ps1
+++ KataStringReplacer.PowerSpec/template.ps1
@@ -2,7 +2,7 @@ function template($text, $tokens = @{}) {
   $tokens.getenumerator() | %{
     $key = $_.key
     $value = $_.value
-    $text = $text.replace("%$key%", "$value")
+    $text = $text -replace "%$key%", "$value"
   }
   $text -replace "%.+?%", ""
 }

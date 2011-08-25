e29c3e076c43906bcd17f5012d8bb82701816955
Ilker Cetinkaya <ilker@ilker.de>
2011-08-25 23:33:59 +0200

    template with unknown key -> text without key : red

--- KataStringReplacer.PowerSpec/template_specs.ps1
+++ KataStringReplacer.PowerSpec/template_specs.ps1
@@ -3,3 +3,4 @@
 template "" | should be_equal ""
 template "anything" | should be_equal "anything"
 template "say %what%" @{"what"="hi"} | should be_equal "say hi"
+template "say %what%" @{"msg"="hi"} | should be_equal "say "

e2a53c00f77dbfda558326346a7e48cb1b102dac
Ilker Cetinkaya <ilker@ilker.de>
2011-11-23 18:36:59 +0100

    corner has 3 neighbors becomes alive : green

--- KataGameOfLife.Mono/GameOfLife.cs
+++ KataGameOfLife.Mono/GameOfLife.cs
@@ -3,15 +3,22 @@ namespace GameOfLife {
 
   public class GOL {
     public static int[,] Spin(int[,] world) {
-      int x = 1, y = 1;
+      for (int x = 0; x < world.GetLength(0); x++) {
+        for (int y = 0; y < world.GetLength(1); y++) {
+          int neighbors = 0;
 
-      int neighbors = 0;
+          for (int ex = x-1; ex <= x+1; ex++) {
+            for (int ey = y-1; ey <= y+1; ey++) {
+              if ((ex >= 0) && (ey >= 0) &&
+                  (ex < world.GetLength(0)) &&
+                  (ey < world.GetLength(1)))
+                neighbors += world[ex, ey];
+            }
+          }
 
-      for (int ex = x-1; ex <= x+1; ex++)
-        for (int ey = y-1; ey <= x+1; ey++)
-          neighbors += world[ex, ey];
-
-      world[1,1] = neighbors == 3 ? 1 : world[1,1];
+          world[x,y] = neighbors == 3 ? 1 : world[1,1];
+        }
+      }
 
       return world;
     }

f02c34a786b622dfed965fa6bdf8e781fc4592ee
Ilker Cetinkaya <ilker@ilker.de>
2011-11-23 18:14:28 +0100

    three neighbors bring back life - red

--- KataGameOfLife.Mono/GameOfLife.cs
+++ KataGameOfLife.Mono/GameOfLife.cs
@@ -1,3 +1,9 @@
 namespace GameOfLife {
   using System;
+
+  public class GOL {
+    public static int[,] Spin(int[,] world) {
+      return world;
+    }
+  }
 }
--- KataGameOfLife.Mono/GameOfLifeTests.cs
+++ KataGameOfLife.Mono/GameOfLifeTests.cs
@@ -1,4 +1,19 @@
 namespace GameOfLife {
   using System;
   using NUnit.Framework;
+
+  [TestFixture]
+  public class when_dead_center_of_3x3_grid_has_exactly_3_neighbors {
+    int[,] world = new int[,]{
+      {0,0,0},
+      {0,0,0},
+      {1,1,1}
+    };
+
+    [Test]
+    public void then_center_becomes_alive() {
+      int[,] nextworld = GOL.Spin(world);
+      Assert.AreEqual(1, nextworld[1,1]);
+    }
+  }
 }

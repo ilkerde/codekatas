272acf9738a8458eb490d0b811ea62c9730d0f03
Ilker Cetinkaya <ilker@ilker.de>
2011-11-23 18:24:12 +0100

    no neighbors keep it same same : green

--- KataGameOfLife.Mono/GameOfLife.cs
+++ KataGameOfLife.Mono/GameOfLife.cs
@@ -3,7 +3,16 @@ namespace GameOfLife {
 
   public class GOL {
     public static int[,] Spin(int[,] world) {
-      world[1,1] = 1;
+      int x = 1, y = 1;
+
+      int neighbors = 0;
+
+      for (int ex = x-1; ex <= x+1; ex++)
+        for (int ey = y-1; ey <= x+1; ey++)
+          neighbors += world[ex, ey];
+
+      world[1,1] = neighbors == 3 ? 1 : world[1,1];
+
       return world;
     }
   }

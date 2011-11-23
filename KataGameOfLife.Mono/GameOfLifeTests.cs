7deacfd8ab4baf3c6a3998462a3002a3cd8f8284
Ilker Cetinkaya <ilker@ilker.de>
2011-11-23 18:27:52 +0100

    corner has 3 neighbors becomes alive : red

--- KataGameOfLife.Mono/GameOfLifeTests.cs
+++ KataGameOfLife.Mono/GameOfLifeTests.cs
@@ -31,4 +31,19 @@ namespace GameOfLife {
       Assert.AreEqual(0, nextworld[1,1]);
     }
   }
+
+  [TestFixture]
+  public class when_dead_corner_of_3x3_grid_has_exactly_3_neighbors {
+    int[,] world = new int[,]{
+      {0,0,0},
+      {1,1,0},
+      {0,1,0}
+    };
+
+    [Test]
+    public void then_corner_becomes_alive() {
+      int[,] nextworld = GOL.Spin(world);
+      Assert.AreEqual(1, nextworld[0,2]);
+    }
+  }
 }

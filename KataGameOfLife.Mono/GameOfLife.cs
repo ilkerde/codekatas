48085bb7a70b50ca1507c8dcaba2d1e95b8d5bf1
Ilker Cetinkaya <ilker@ilker.de>
2011-11-23 19:05:35 +0100

    extracted neighbor scan - blue

--- KataGameOfLife.Mono/GameOfLife.cs
+++ KataGameOfLife.Mono/GameOfLife.cs
@@ -8,22 +8,30 @@ namespace GameOfLife {
 
       for (int x = 0; x < xMax; x++) {
         for (int y = 0; y < yMax; y++) {
-          int neighbors = 0;
-
-          for (int ex = x-1; ex <= x+1; ex++) {
-            for (int ey = y-1; ey <= y+1; ey++) {
-              if ((ex >= 0) && (ey >= 0) &&
-                  (ex < xMax) &&
-                  (ey < yMax))
-                neighbors += world[ex, ey];
-            }
-          }
-
+          int neighbors = GetNumberOfNeighbors(x, y, world);
           world[x,y] = neighbors == 3 ? 1 : world[1,1];
         }
       }
 
       return world;
+    }
+
+    private static int GetNumberOfNeighbors(int x, int y, int[,] world) {
+      int xMax = world.GetLength(0);
+      int yMax = world.GetLength(1);
+
+      int neighbors = 0;
+
+      for (int ex = x-1; ex <= x+1; ex++) {
+        for (int ey = y-1; ey <= y+1; ey++) {
+          if ((ex >= 0) && (ey >= 0) &&
+              (ex < xMax) &&
+              (ey < yMax))
+            neighbors += world[ex, ey];
+        }
+      }
+
+      return neighbors;
     }
   }
 }

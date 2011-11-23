c5405565b2bf1d88fec5874470eb3a019d0b44a9
Ilker Cetinkaya <ilker@ilker.de>
2011-11-23 19:48:31 +0100

    cell with 2 friends stays alive : red

--- KataGameOfLife.Mono/GameOfLifeTests.cs
+++ KataGameOfLife.Mono/GameOfLifeTests.cs
@@ -82,4 +82,19 @@ namespace GameOfLife {
       Assert.AreEqual(0, nextworld[1,1]);
     }
   }
+
+  [TestFixture]
+  public class when_alive_center_of_3x3_grid_has_two_neighbors {
+    int[,] world = new int[,]{
+      {0,0,0},
+      {1,1,1},
+      {0,0,0}
+    };
+
+    [Test]
+    public void then_center_stays_alive() {
+      int[,] nextworld = GOL.Spin(world);
+      Assert.AreEqual(1, nextworld[1,1]);
+    }
+  }
 }

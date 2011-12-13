b97fadf424e213ab236afc2c045b52366a7e6db5
Ilker Cetinkaya <ilker@ilker.de>
2011-12-13 09:08:19 +0100

    empty upper left with no neighbors stays empty : green

--- KataGameOfLife.RSpec/lib/gol.rb
+++ KataGameOfLife.RSpec/lib/gol.rb
@@ -1,6 +1,15 @@
 class Gol
   def spin world
-    world[0,0] = 1
+    x = 0
+    y = 0
+
+    n1 = world[x+1][y]
+    n2 = world[x+1][y+1]
+    n3 = world[x][y+1]
+
+    n = n1+n2+n3
+    world[x][y] = 1 if n == 3
+
     world
   end
 end

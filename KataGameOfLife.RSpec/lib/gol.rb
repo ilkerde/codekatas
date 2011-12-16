89859c57eb99a4d00d32f85d3a0cc12bec6763ae
Ilker Cetinkaya <ilker@ilker.de>
2011-12-16 15:37:23 +0100

    lower right with 3 neighbors brings life : green

--- KataGameOfLife.RSpec/lib/gol.rb
+++ KataGameOfLife.RSpec/lib/gol.rb
@@ -1,17 +1,34 @@
 class Gol
   def spin world
-    x = 0
-    y = 0
-    n = 0
+    newworld = world
 
-    (x..x+1).each do |nx|
-      (y..y+1).each do |ny|
-        n += world[nx][ny]
+    mx = world.length
+    my = world[0].length
+
+    (0..mx).each do |x|
+      (0..my).each do |y|
+        lox = x-1
+        loy = y-1
+        hix = x+1
+        hiy = x+1
+
+        hix = world.length - 1 if hix >= world.length
+        hiy = world[hix].length - 1 if hiy >= world[hix].length
+        lox = 0 if lox < 0
+        loy = 0 if loy < 0
+
+        n = 0
+        
+        (lox..hix).each do |nx|
+          (loy..hiy).each do |ny|
+            n += world[nx][ny]
+          end
+        end
+
+        newworld[x][y] = 1 if n == 3
       end
     end
 
-    world[x][y] = 1 if n == 3
-
-    world
+    newworld
   end
 end

f08bb82f40d619370fd051a9f5416c54720e0a1f
Ilker Cetinkaya <ilker@ilker.de>
2011-12-20 23:10:16 +0100

    repopulation rule, empty cell with three neighbors is just empty : green

--- KataGameOfLife.Nose/gol.py
+++ KataGameOfLife.Nose/gol.py
@@ -3,11 +3,10 @@ class Gol:
     return cell.next_generation()
 
 class Cell:
-  def __init__(self, neighbors=[]):
+  def __init__(self, neighbors=[], is_alive=False):
     self.neighbors = neighbors
-
-  def is_alive(self):
-    return len(self.neighbors) == 3
+    self.is_alive = is_alive
 
   def next_generation(self):
-    return self
+    will_be_alive = len(self.neighbors) == 3
+    return Cell(self.neighbors, will_be_alive)
--- KataGameOfLife.Nose/gol_test.py
+++ KataGameOfLife.Nose/gol_test.py
@@ -7,14 +7,14 @@ class test_repopulation_rule:
     neighbors = [Cell(), Cell(), Cell()]
     cell = Cell(neighbors)
     new_cell = self.gol.generate(cell)
-    assert new_cell.is_alive()
+    assert new_cell.is_alive
 
   def when_empty_cell_has_three_neighbors_its_just_empty_test(self):
     neighbors = [Cell(), Cell(), Cell()]
     cell = Cell(neighbors)
-    assert not cell.is_alive()
+    assert not cell.is_alive
 
   def when_empty_cell_has_no_neighbors_then_cell_stays_empty_test(self):
     cell = Cell()
     new_cell = self.gol.generate(cell)
-    assert not new_cell.is_alive()
+    assert not new_cell.is_alive

e98d2260cbb4f9d81fbfc7eba86d92552e4745e1
Ilker Cetinkaya <ilker@ilker.de>
2011-12-20 22:40:50 +0100

    repopulation rule, empty cell with three neighbors is just empty : red

--- KataGameOfLife.Nose/gol_test.py
+++ KataGameOfLife.Nose/gol_test.py
@@ -6,14 +6,15 @@ class test_repopulation_rule:
   def when_empty_cell_has_three_neighbors_then_cell_gets_populated_test(self):
     neighbors = [Cell(), Cell(), Cell()]
     cell = Cell(neighbors)
-    
     new_cell = self.gol.generate(cell)
-
     assert new_cell.is_alive()
+
+  def when_empty_cell_has_three_neighbors_its_just_empty_test(self):
+    neighbors = [Cell(), Cell(), Cell()]
+    cell = Cell(neighbors)
+    assert not cell.is_alive()
 
   def when_empty_cell_has_no_neighbors_then_cell_stays_empty_test(self):
     cell = Cell()
-
     new_cell = self.gol.generate(cell)
-
     assert not new_cell.is_alive()

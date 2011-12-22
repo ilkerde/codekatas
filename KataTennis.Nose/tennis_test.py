fa0e1325bbb2712be2a131d15fbb139bc3da60f1
Ilker Cetinkaya <ilker@ilker.de>
2011-12-22 08:26:40 +0100

    score 0:0 -> score p1 -> game not over : red

--- KataTennis.Nose/tennis_test.py
+++ KataTennis.Nose/tennis_test.py
@@ -9,3 +9,8 @@ def when_score_is_forty_love_and_p1_score_then_game_is_over_test():
   tennis = TennisGame(40, 0)
   tennis.score_player_1()
   assert tennis.is_over == True
+
+def when_score_is_love_all_and_p1_score_then_game_is_not_over_test():
+  tennis = TennisGame()
+  tennis.score_player_1()
+  assert not tennis.is_over

57d35ff4ea8fc7edabf1e673c6226270fb050d73
Ilker Cetinkaya <ilker@ilker.de>
2011-12-22 08:24:13 +0100

    score 40:0 -> score p1 -> game over : red

--- KataTennis.Nose/tennis.py
+++ KataTennis.Nose/tennis.py
@@ -1,2 +1,9 @@
 class TennisGame:
-  is_over=False
+  is_over = False
+  
+  def __init__(self, score_p1=0, score_p2=0):
+    self.score = (score_p1, score_p2)
+
+  def score_player_1(self):
+    pass
+
--- KataTennis.Nose/tennis_test.py
+++ KataTennis.Nose/tennis_test.py
@@ -1,6 +1,11 @@
 from tennis import TennisGame
 
-tennis = TennisGame()
 
 def when_score_is_love_all_then_game_is_not_over_test():
+  tennis = TennisGame()
   assert tennis.is_over == False
+
+def when_score_is_forty_love_and_p1_score_then_game_is_over_test():
+  tennis = TennisGame(40, 0)
+  tennis.score_player_1()
+  assert tennis.is_over == True

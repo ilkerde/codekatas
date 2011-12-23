f118d265bd2ce2f5d6d741d1144b4780106b9f8a
Ilker Cetinkaya <ilker@ilker.de>
2011-12-23 17:46:15 +0100

    grid system, cells adjacent to x-axis become neighbors : red

--- KataGameOfLife.Nose/gol_test.py
+++ KataGameOfLife.Nose/gol_test.py
@@ -85,3 +85,10 @@ class test_gol_cell_position:
     g.full_cell((2,1))
     assert g.check_cell((1,1)) in g.check_cell((2,1)).neighbors
     assert g.check_cell((2,1)) in g.check_cell((1,1)).neighbors
+
+  def when_a_cell_is_added_to_previous_x_axis_position_of_an_existing_cell_then_both_become_neighbors_test(self):
+    g = Gol()
+    g.full_cell((2,1))
+    g.full_cell((1,1))
+    assert g.check_cell((1,1)) in g.check_cell((2,1)).neighbors
+    assert g.check_cell((2,1)) in g.check_cell((1,1)).neighbors

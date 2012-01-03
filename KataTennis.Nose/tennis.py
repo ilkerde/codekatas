bf694e38b2b814e562366e3b929dfec38030f882
Ilker Cetinkaya <ilker@ilker.de>
2012-01-03 23:28:50 +0100

    score 0:0 + p1 -> score 15:0 : green

--- KataTennis.Nose/tennis.py
+++ KataTennis.Nose/tennis.py
@@ -1,3 +1,11 @@
+class Score:
+  LOVE = 0
+  FIFTEEN = 15
+  THIRTY = 30
+  FORTY = 40
+  ADVANTAGE = 50
+  GAME = 100
+
 class TennisGame:
   is_over = False
   
@@ -5,4 +13,21 @@ class TennisGame:
     self.score = (score_p1, score_p2)
 
   def score_player(self, player):
-    self.is_over = self.score[player-1] == 40
+    self.score = tuple(self.inc(x) if i+1==player else x for i,x in enumerate(self.score))
+    self.is_over = self.score[player-1] == 50
+
+  def inc(self, score):
+    next_offset = self._scoretable.index(score) + 1
+    score = self._scoretable[next_offset]
+    print score
+    return score
+
+  _scoretable = [
+    Score.LOVE,
+    Score.FIFTEEN,
+    Score.THIRTY,
+    Score.FORTY,
+    Score.ADVANTAGE,
+    Score.GAME
+  ]
+

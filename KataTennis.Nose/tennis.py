8a41349afcc0dfaa6b40e523d8fff9d47bf8d27f
Ilker Cetinkaya <ilker@ilker.de>
2012-01-04 00:01:36 +0100

    refactored TennisGame.inc to Score.next static. more readable positioning of list comprehension. eliminated ADVANTAGE since not required yet.

--- KataTennis.Nose/tennis.py
+++ KataTennis.Nose/tennis.py
@@ -3,31 +3,27 @@ class Score:
   FIFTEEN = 15
   THIRTY = 30
   FORTY = 40
-  ADVANTAGE = 50
   GAME = 100
+
+  _table = [LOVE, FIFTEEN, THIRTY, FORTY, GAME]
+
+  @staticmethod
+  def next(score):
+    if score == Score.GAME:
+      return score
+
+    return Score._table[Score._table.index(score) + 1]
 
 class TennisGame:
   is_over = False
   
   def __init__(self, score_p1=0, score_p2=0):
-    self.score = (score_p1, score_p2)
+    self.score = score_p1, score_p2
 
   def score_player(self, player):
-    self.score = tuple(self.inc(x) if i+1==player else x for i,x in enumerate(self.score))
-    self.is_over = self.score[player-1] == 50
-
-  def inc(self, score):
-    next_offset = self._scoretable.index(score) + 1
-    score = self._scoretable[next_offset]
-    print score
-    return score
-
-  _scoretable = [
-    Score.LOVE,
-    Score.FIFTEEN,
-    Score.THIRTY,
-    Score.FORTY,
-    Score.ADVANTAGE,
-    Score.GAME
-  ]
-
+    self.score = tuple(
+      Score.next(x)
+      if i + 1 == player else x 
+      for i, x in enumerate(self.score)
+    )
+    self.is_over = self.score[player-1] == Score.GAME

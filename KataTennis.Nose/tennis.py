4883dee733d3697669a12cfff407b84cec817c1a
Ilker Cetinkaya <ilker@ilker.de>
2012-01-06 12:40:15 +0100

    score 40:40 + p1 -> not over : green

--- KataTennis.Nose/tennis.py
+++ KataTennis.Nose/tennis.py
@@ -3,9 +3,10 @@ class Score:
   FIFTEEN = 15
   THIRTY = 30
   FORTY = 40
+  ADVANTAGE = 50
   GAME = 100
 
-  _table = [LOVE, FIFTEEN, THIRTY, FORTY, GAME]
+  _table = [LOVE, FIFTEEN, THIRTY, FORTY, ADVANTAGE, GAME]
 
   @staticmethod
   def next(score):
@@ -14,6 +15,12 @@ class Score:
 
     return Score._table[Score._table.index(score) + 1]
 
+  @staticmethod
+  def is_two_ahead(score_a, score_b):
+    offset_a = Score._table.index(score_a)
+    offset_b = Score._table.index(score_b)
+    return abs(offset_a - offset_b) > 1
+
 class TennisGame:
   is_over = False
   
@@ -21,9 +28,13 @@ class TennisGame:
     self.score = score_p1, score_p2
 
   def score_player(self, player):
+    scorer = player - 1;
+    looser = 1 - scorer;
+
     self.score = tuple(
       Score.next(x)
-      if i + 1 == player else x 
+      if i == scorer else x 
       for i, x in enumerate(self.score)
     )
-    self.is_over = self.score[player-1] == Score.GAME
+
+    self.is_over = Score.is_two_ahead(self.score[scorer], self.score[looser])

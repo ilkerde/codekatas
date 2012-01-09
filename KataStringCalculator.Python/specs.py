640e9a8486ff8cc34517c1e876d1cce40b95de35
Ilker Cetinkaya <ilker@ilker.de>
2012-01-09 14:15:57 +0100

    numbers w/ newline + comma -> sum : blue

--- KataStringCalculator.Python/specs.py
+++ KataStringCalculator.Python/specs.py
@@ -19,3 +19,7 @@ class test_when_arbitrary_number_of_numbers_are_given:
 class test_when_numbers_are_separated_by_newline_are_given:
   def test_then_returns_sum_of_numbers(self):
     assert scal_add("3\n5") == 8
+
+class test_when_numbers_are_separated_by_newline_and_comma_are_given:
+  def test_then_returns_sum_of_numbers(self):
+    assert scal_add("3\n5,8") == 16

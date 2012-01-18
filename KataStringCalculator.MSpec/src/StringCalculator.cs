d6f4c84afc249dbdb009130e50c8106aa148ae5b
Ilker Cetinkaya <ilker@ilker.de>
2012-01-18 13:20:50 +0100

    The string calculator with machine specifications.

---
+++ KataStringCalculator.MSpec/src/StringCalculator.cs
@@ -0,0 +1,9 @@
+namespace Kata {
+  using System;
+
+  public static class StringCalculator {
+    public static int Add(string expression) {
+      return 0;
+    }
+  }
+}
---
+++ KataStringCalculator.MSpec/src/StringCalculatorSpecs.cs
@@ -0,0 +1,8 @@
+namespace Kata {
+  using System;
+  using Machine.Specifications;
+
+  class When_having_numbers_separated_by_comma {
+    It should_return_the_sum_of_both_numbers;
+  }
+}

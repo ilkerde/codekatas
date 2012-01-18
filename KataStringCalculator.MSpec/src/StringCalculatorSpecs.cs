d642b000c1ab71c918bb1317dca0399552c87e48
Ilker Cetinkaya <ilker@ilker.de>
2012-01-18 13:24:56 +0100

    single number -> number : red

--- KataStringCalculator.MSpec/src/StringCalculatorSpecs.cs
+++ KataStringCalculator.MSpec/src/StringCalculatorSpecs.cs
@@ -6,4 +6,9 @@ namespace Kata {
     It should_return_the_sum_of_both_numbers = () 
       => StringCalculator.Add("2,2").ShouldEqual(4); 
   }
+
+  class When_having_just_a_single_number {
+    It should_return_just_the_number = ()
+      => StringCalculator.Add("3").ShouldEqual(3);
+  }
 }

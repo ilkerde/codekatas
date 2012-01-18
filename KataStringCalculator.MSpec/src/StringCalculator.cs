f66036e9dfa234c2916eaa6e97831a0367395ab3
Ilker Cetinkaya <ilker@ilker.de>
2012-01-18 16:14:15 +0100

    touchdown. minor refactoring with extract method on separator quire. did not fully pass specs of step 1 for calculator - the negative number checking is missing - since mspec runner fails fatal with catch.exception() expression on mono. will probably investivate later on. the kata was straightforward an i felt mspec was giving me the right expressiveness for it.

--- KataStringCalculator.MSpec/src/StringCalculator.cs
+++ KataStringCalculator.MSpec/src/StringCalculator.cs
@@ -7,20 +7,28 @@ namespace Kata {
       if (string.IsNullOrEmpty(expression))
         return 0;
 
-      var separators = new List<char> { ',', '\n' };
+      char[] separators;
+      expression = AquireSeparators(expression, out separators);
 
-      if (expression.StartsWith("//")) {
-        separators.Add(expression[2]);
-        expression = expression.Substring(expression.IndexOf('\n') + 1);
-      }
-
-      string[] parts = expression.Split(separators.ToArray());
+      string[] parts = expression.Split(separators);
 
       int result = 0;
       foreach (var part in parts)
         result += Convert.ToInt32(part);
 
       return result;
+    }
+
+    private static string AquireSeparators(string expression, out char[] separators) {
+      var separatorList = new List<char> { ',', '\n' };
+
+      if (expression.StartsWith("//")) {
+        separatorList.Add(expression[2]);
+        expression = expression.Substring(expression.IndexOf('\n') + 1);
+      }
+
+      separators = separatorList.ToArray();
+      return expression;
     }
   }
 }

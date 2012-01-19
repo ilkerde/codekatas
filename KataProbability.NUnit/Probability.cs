471924ee59b40288a59dcca9aa71ba8e906d47c6
Ilker Cetinkaya <ilker@ilker.de>
2012-01-19 14:15:53 +0100

    1 inverted = 0 : green

--- KataProbability.NUnit/Probability.cs
+++ KataProbability.NUnit/Probability.cs
@@ -1,3 +1,28 @@
 namespace Kata {
   using System;
+
+  public class Probability
+    : IEquatable<Probability> {
+    decimal value;
+    
+    public Probability(decimal value) {
+      this.value = value;
+    }
+
+    public Probability InverseOf() {
+      return new Probability(0);
+    }
+
+    public bool Equals(Probability other) {
+      return other != null && other.value.Equals(value);
+    }
+
+    public override bool Equals(object other) {
+      return Equals(other as Probability);
+    }
+
+    public override int GetHashCode() {
+      return value.GetHashCode();
+    }
+  }
 }

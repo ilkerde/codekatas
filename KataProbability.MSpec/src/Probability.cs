f810d913885c2dfb1d5975284d2e650a947d56b4
Ilker Cetinkaya <ilker@ilker.de>
2012-01-19 10:33:23 +0100

    1.inverseof -> 0 : red

--- KataProbability.MSpec/src/Probability.cs
+++ KataProbability.MSpec/src/Probability.cs
@@ -1,3 +1,15 @@
 namespace Kata {
   using System;
+
+  class Probability {
+    decimal value;
+
+    public Probability(decimal initialValue) {
+      value = initialValue;
+    }
+
+    public Probability InverseOf() {
+      return null;
+    }
+  }
 }
--- KataProbability.MSpec/src/ProbabilitySpecs.cs
+++ KataProbability.MSpec/src/ProbabilitySpecs.cs
@@ -1,4 +1,10 @@
 namespace Kata {
   using System;
   using Machine.Specifications;
+
+  class When_the_inversion_prob_of_prob_1_is_required {
+    It should_return_0 = () => new Probability(1)
+      .InverseOf()
+      .ShouldEqual(new Probability(0));
+  }
 }

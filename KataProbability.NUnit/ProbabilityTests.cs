3902eb250f04f742350999a3eb142727cf4c5ed4
Ilker Cetinkaya <ilker@ilker.de>
2012-01-19 14:15:32 +0100

    1 inverted = 0 : red

--- KataProbability.NUnit/ProbabilityTests.cs
+++ KataProbability.NUnit/ProbabilityTests.cs
@@ -1,4 +1,13 @@
 namespace Kata {
   using System;
   using NUnit.Framework;
+
+  [TestFixture] public class When_the_inverse_probability_of_a_probability_is_required {
+    [Test] public void Then_a_probability_of_1_should_invert_to_0() {
+      Assert.AreEqual(
+        new Probability(1).InverseOf(),
+        new Probability(0)
+      );
+    }
+  }
 }

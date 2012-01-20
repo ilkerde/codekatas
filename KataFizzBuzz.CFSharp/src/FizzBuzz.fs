354387da33ef4c43b70afa23216908e20921837c
Ilker Cetinkaya <ilker@ilker.de>
2012-01-20 23:35:25 +0100

    range 1..30 -> list : green

--- KataFizzBuzz.CFSharp/src/FizzBuzz.fs
+++ KataFizzBuzz.CFSharp/src/FizzBuzz.fs
@@ -1,5 +1,14 @@
 namespace Kata
 
+open System
+open System.Collections.Generic
+
+type Translation = 
+  {
+    Source :int;
+    Target :string
+  }
+
 module FizzBuzz =
   let translate number =
     let fizz = if number % 3 = 0 then "Fizz" else ""
@@ -9,3 +18,11 @@ module FizzBuzz =
     let snumber = sprintf "%d" number
 
     if fizzbuzz = "" then snumber else fizzbuzz
+
+  let translateRange start stop = 
+    new List<Translation>(
+      seq { 
+        for number in start .. stop do 
+        yield { Source = 0; Target = "" }
+      }
+    )

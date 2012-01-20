db75b5c9ac795941f23815e070125fd3d4b01158
Ilker Cetinkaya <ilker@ilker.de>
2012-01-20 22:27:14 +0100

    multiples of 3 and 5 -> 'FizzBuzz' : green

--- KataFizzBuzz.CFSharp/src/FizzBuzz.fs
+++ KataFizzBuzz.CFSharp/src/FizzBuzz.fs
@@ -1,5 +1,8 @@
 namespace Kata
 
 module FizzBuzz =
-  let translate number = 
-    if number % 5 = 0 then "Buzz" else "Fizz"
+  let translate number =
+    let fizz = if number % 3 = 0 then "Fizz" else ""
+    let buzz = if number % 5 = 0 then "Buzz" else ""
+
+    fizz + buzz

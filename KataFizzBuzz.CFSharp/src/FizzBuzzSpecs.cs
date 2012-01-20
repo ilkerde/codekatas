4127d8e6e400e37854b35d261e06807959e952f7
Ilker Cetinkaya <ilker@ilker.de>
2012-01-20 22:11:36 +0100

    multiples of 5 -> 'Buzz' : red

--- KataFizzBuzz.CFSharp/src/FizzBuzzSpecs.cs
+++ KataFizzBuzz.CFSharp/src/FizzBuzzSpecs.cs
@@ -6,4 +6,9 @@ namespace Kata {
     It should_translate_to__Fizz__ = 
       () => FizzBuzz.translate(6).ShouldEqual("Fizz");
   }
+
+  class when_number_is_multiples_of_five {
+    It should_translate_to__Buzz__ = 
+      () => FizzBuzz.translate(10).ShouldEqual("Buzz");
+  }
 }

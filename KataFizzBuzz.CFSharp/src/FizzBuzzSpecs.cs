4c67bdd9f451762bb3c0b6261ad048518182f688
Ilker Cetinkaya <ilker@ilker.de>
2012-01-20 22:10:16 +0100

    multiples of 3 -> 'Fizz' : red

--- KataFizzBuzz.CFSharp/src/FizzBuzzSpecs.cs
+++ KataFizzBuzz.CFSharp/src/FizzBuzzSpecs.cs
@@ -1,4 +1,9 @@
 namespace Kata {
   using System;
   using Machine.Specifications;
+
+  class when_number_is_multiples_of_three {
+    It should_translate_to__Fizz__ = 
+      () => FizzBuzz.translate(6).ShouldEqual("Fizz");
+  }
 }

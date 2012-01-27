35cf8117102b6bc9f82814cb39976a00e779f5a9
Ilker Cetinkaya <ilker@ilker.de>
2012-01-27 11:37:45 +0100

    keyword => replace : red

--- KataStringReplacer.FSharp/src/TemplaterTests.fs
+++ KataStringReplacer.FSharp/src/TemplaterTests.fs
@@ -13,3 +13,8 @@ let should_yield_empty_text_when_empty_text_is_provided () =
 let should_yield_text_when_text_is_passed_with_no_keywords () = 
   let text = subst "something" []
   Assert.Equal<string>("something", text)
+
+[<Fact>]
+let should_replace_key_with_value_when_key_was_found () = 
+  let text = subst "hi $who$" [("who", "bingo")]
+  Assert.Equal<string>("hi bingo", text)

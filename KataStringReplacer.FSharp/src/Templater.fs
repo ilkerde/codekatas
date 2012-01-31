a6b86a5e465d434cb1c43ae345dc0e3a198c5396
Ilker Cetinkaya <ilker@ilker.de>
2012-01-31 11:06:49 +0100

    more keywords => replace all : green

--- KataStringReplacer.FSharp/src/Templater.fs
+++ KataStringReplacer.FSharp/src/Templater.fs
@@ -3,17 +3,12 @@ module Templater
 open System
 
 let subst (text:string) (values:(string * string) list) = 
-  let firstSet (values:(string*string) list) =
-    match values with
-    | head::tail -> head
-    | [] -> ("any","any")
-
   let createKey = sprintf "$%s$"
 
   let replaceSet (text:string) (set:string * string) =
     let k,v = set
     text.Replace(createKey k,v)
 
-  replaceSet text (firstSet values)
-
+  values
+  |> List.fold replaceSet text
 

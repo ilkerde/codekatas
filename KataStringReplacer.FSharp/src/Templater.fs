ac8c93d4de4161f802685ed67529afadea579014
Ilker Cetinkaya <ilker@ilker.de>
2012-01-31 17:41:59 +0100

    unknown keyword => wipe : green

--- KataStringReplacer.FSharp/src/Templater.fs
+++ KataStringReplacer.FSharp/src/Templater.fs
@@ -9,6 +9,24 @@ let subst (text:string) (values:(string * string) list) =
     let k,v = set
     text.Replace(createKey k,v)
 
-  values
-  |> List.fold replaceSet text
+  let (|AnyKey|_|) (text:string) = 
+    let lpos = text.IndexOf("$")
+    if lpos < 0 then
+      None
+    else
+      let rpos = text.IndexOf("$", lpos + 1)
+      if rpos > lpos then
+        Some (text.Substring(lpos, rpos - lpos + 1))
+      else
+        None
 
+  let rec wipeKey (text:string) = 
+    match text with
+    | AnyKey(k) -> wipeKey (text.Replace(k, ""))
+    | _ -> text
+
+  let newText = 
+    values
+    |> List.fold replaceSet text
+
+  wipeKey newText

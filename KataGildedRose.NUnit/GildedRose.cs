76c991f9bb95fadd04849541cb295363b67c15f5
Ilker Cetinkaya <ilker@ilker.de>
2012-05-16 16:53:40 +0200

    replace item symbol in store update method.

--- KataGildedRose.NUnit/GildedRose.cs
+++ KataGildedRose.NUnit/GildedRose.cs
@@ -56,72 +56,72 @@ namespace Kata {
           var item = Items[i];
           _updateItemQuality(item);
 
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].Quality > 0)
+                if (item.Quality > 0)
                 {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        Items[i].Quality = Items[i].Quality - 1;
+                        item.Quality = item.Quality - 1;
                     }
                 }
             }
             else
             {
-                if (Items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
+                    item.Quality = item.Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].SellIn < 11)
+                        if (item.SellIn < 11)
                         {
-                            if (Items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                Items[i].Quality = Items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
 
-                        if (Items[i].SellIn < 6)
+                        if (item.SellIn < 6)
                         {
-                            if (Items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                Items[i].Quality = Items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
             {
-                Items[i].SellIn = Items[i].SellIn - 1;
+                item.SellIn = item.SellIn - 1;
             }
 
-            if (Items[i].SellIn < 0)
+            if (item.SellIn < 0)
             {
-                if (Items[i].Name != "Aged Brie")
+                if (item.Name != "Aged Brie")
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Quality > 0)
+                        if (item.Quality > 0)
                         {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                Items[i].Quality = Items[i].Quality - 1;
+                                item.Quality = item.Quality - 1;
                             }
                         }
                     }
                     else
                     {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        item.Quality = item.Quality - item.Quality;
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }

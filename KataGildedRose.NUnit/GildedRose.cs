857bcdcf8e2729a55d501d18623a67a8633cd8d8
Ilker Cetinkaya <ilker@ilker.de>
2012-05-16 16:57:17 +0200

    move degradation logic to single item update.

--- KataGildedRose.NUnit/GildedRose.cs
+++ KataGildedRose.NUnit/GildedRose.cs
@@ -46,92 +46,88 @@ namespace Kata {
     private Func<Item, Item> _updateItemQuality;
 
     public static Item UpdateItemQuality(Item item) {
+      if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+      {
+          if (item.Quality > 0)
+          {
+              if (item.Name != "Sulfuras, Hand of Ragnaros")
+              {
+                  item.Quality = item.Quality - 1;
+              }
+          }
+      }
+      else
+      {
+          if (item.Quality < 50)
+          {
+              item.Quality = item.Quality + 1;
+
+              if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+              {
+                  if (item.SellIn < 11)
+                  {
+                      if (item.Quality < 50)
+                      {
+                          item.Quality = item.Quality + 1;
+                      }
+                  }
+
+                  if (item.SellIn < 6)
+                  {
+                      if (item.Quality < 50)
+                      {
+                          item.Quality = item.Quality + 1;
+                      }
+                  }
+              }
+          }
+      }
+
+      if (item.Name != "Sulfuras, Hand of Ragnaros")
+      {
+          item.SellIn = item.SellIn - 1;
+      }
+
+      if (item.SellIn < 0)
+      {
+          if (item.Name != "Aged Brie")
+          {
+              if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+              {
+                  if (item.Quality > 0)
+                  {
+                      if (item.Name != "Sulfuras, Hand of Ragnaros")
+                      {
+                          item.Quality = item.Quality - 1;
+                      }
+                  }
+              }
+              else
+              {
+                  item.Quality = item.Quality - item.Quality;
+              }
+          }
+          else
+          {
+              if (item.Quality < 50)
+              {
+                  item.Quality = item.Quality + 1;
+              }
+          }
+      }
+
       return item;
     }
 
-    public void UpdateQuality()
-    {
-        for (var i = 0; i < Items.Count; i++)
-        {
-          var item = Items[i];
-          _updateItemQuality(item);
-
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
-        }
+    public void UpdateQuality() {
+      for (var i = 0; i < Items.Count; i++) {
+        var item = Items[i];
+        _updateItemQuality(item);
+      }
     }
-
   }
 
-  public class Item
-  {
+  public class Item {
     public string Name { get; set; }
     public int SellIn { get; set; }
     public int Quality { get; set; }

0961e326ad0de687d69a7bb973ed80ad5e27bf8a
Ilker Cetinkaya <ilker@ilker.de>
2012-05-20 18:32:55 +0200

    tickets quality increases by two if 10 >= sellin : green

--- KataGildedRose.NUnit/Item_Update_Tests.cs
+++ KataGildedRose.NUnit/Item_Update_Tests.cs
@@ -97,6 +97,17 @@ namespace Kata.Item_Tests {
     }
   }
 
+  [TestFixture]
+  public class When_Item_Is_Tickets_And_SellIn_Is_Between_6_And_10_Days_Ahead {
+    [Test]
+    public void Then_Quality_Increases_By_Two() {
+      N_.ConcertTickets
+        .WithQuality(1)
+        .AndSellIn(10)
+        .ShouldHaveQuality(3);
+    }
+  }
+
   public static class ItemTestExtensions {
     public static Item WithQuality(this string name, int quality) {
       return new Item(name) { Quality = quality };

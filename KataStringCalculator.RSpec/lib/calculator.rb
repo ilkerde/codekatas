ee9070c2ae626e043ecbb6e221fefdb5c2a3e89f
Ilker Cetinkaya <ilker@ilker.de>
2012-06-15 17:17:43 +0200

    touchdown. a nice little refreshment on ruby and rspec. I still kind of dont like all this do-end-do-end arrows. Ntl, ruby is indeed a nice language with alot of 'ninja move' potentials. Not sure if I like this in larger projects.

--- KataStringCalculator.RSpec/lib/calculator.rb
+++ KataStringCalculator.RSpec/lib/calculator.rb
@@ -1,17 +1,33 @@
+def convert_to_numbers parts
+  parts.map {|x| x.to_i }
+end
+
+def sum_of numbers
+  numbers.reduce {|s,n| s+n}
+end
+
+def remove_delimiter_definition_of numbers
+  numbers.gsub! /^\/\/.+?\n/, '' 
+end
+
+def parse_delimiter_definition numbers
+  return *(/^\/\/(.+)?\n.+/.match numbers)
+end
+
 class Calculator
   def self.add numbers
     return 0 if numbers == ""
 
-    match, delimiter = *(/^\/\/(.+)?\n.+/.match numbers)
+    match, delimiter = parse_delimiter_definition numbers
     delimiters = Regexp.new "[#{delimiter},\n]"
-    numbers.gsub! /^\/\/.+?\n/, '' unless match.nil?
+    remove_delimiter_definition_of numbers unless match.nil?
 
     parts = numbers.split delimiters
-    nums = parts.map {|x| x.to_i }
+    nums = convert_to_numbers parts
 
     negatives = nums.select {|x| x < 0}
     raise "Negatives not allowed. #{negatives.join ','}" unless negatives.empty?
 
-    nums.reduce {|s,n| s+n}
+    sum_of nums
   end
 end

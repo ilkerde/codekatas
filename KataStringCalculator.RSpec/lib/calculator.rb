968e439b26c563e62c8d2ca3e2aeb3f364708ee6
Ilker Cetinkaya <ilker@ilker.de>
2012-06-15 16:03:59 +0200

    3\n5 -> 8 : green

--- KataStringCalculator.RSpec/lib/calculator.rb
+++ KataStringCalculator.RSpec/lib/calculator.rb
@@ -2,7 +2,7 @@ class Calculator
   def self.add numbers
     return 0 if numbers == ""
 
-    parts = numbers.split ','
+    parts = numbers.split /[,\n]+/
     nums = parts.map {|x| x.to_i }
 
     nums.reduce {|s,n| s+n}

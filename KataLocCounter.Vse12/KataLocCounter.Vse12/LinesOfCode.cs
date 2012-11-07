177226bb6276a6f94f90f8fb99018217fb3a77d1
ilkerde <ilker@ilker.de>
2012-11-07 21:23:39 +0100

    The Loc Counter with a bare VSE12.

---
+++ KataLocCounter.Vse12/KataLocCounter.Vse12/LinesOfCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLocCounter.Vse12
+{
+    public class LinesOfCode
+    {
+        public static int Count(string sourceCode)
+        {
+            return 0;
+        }
+    }
+}
---
+++ KataLocCounter.Vse12/KataLocCounter.Vse12/LinesOfCodeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataLocCounter.Vse12
+{
+    [TestClass]
+    public class LinesOfCodeTests
+    {
+        [TestMethod]
+        public void Empty_String_Does_Not_Count()
+        {
+            string sourceCode = String.Empty;
+            int loc = LinesOfCode.Count(sourceCode);
+            Assert.AreEqual(0, loc);
+        }
+    }
+}

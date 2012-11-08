72fdd0eb71b96976ea7c3c6ec94ea60836a110aa
ilkerde <ilker@ilker.de>
2012-11-08 01:26:41 +0100

    Touchdown. Intentional 'Bad Dev' behaviors actually revealed nice insights. Besides, VSE12 is a nice progress, but doesn't add much value to me in first instance. It definitely lacks a few core refactorings and VIM mode. Touchdown action: Organized tests.

--- KataLocCounter.Vse12/KataLocCounter.Vse12/LinesOfCodeCountTests/Multiline_Comment_Scenarios.cs
+++ KataLocCounter.Vse12/KataLocCounter.Vse12/LinesOfCodeCountTests/Multiline_Comment_Scenarios.cs
@@ -10,25 +10,8 @@ namespace KataLocCounter.Vse12
     .LinesOfCodeCountTests
 {
     [TestClass]
-    public class Line_Exclusion_Scenarioss
+    public class Multiline_Comment_Scenarioss
     {
-        [TestMethod]
-        public void Comment_Line_Does_Not_Count()
-        {
-            LinesOfCode.Count("// test")
-                .ShouldBe(0);
-        }
-
-        [TestMethod]
-        public void Blank_Line_Does_Not_Count()
-        {
-            LinesOfCode.Count(@"// test
-using System;
-
-using System.Text;")
-                .ShouldBe(2);
-        }
-
         [TestMethod]
         public void Multi_Comment_Line_Does_Not_Count()
         {

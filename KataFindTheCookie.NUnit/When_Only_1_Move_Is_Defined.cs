d1e5ca9bb804ae3e6a0af5258fcd6ea5b5e17254
Ilker Cetinkaya <ilker@ilker.de>
2013-07-16 00:07:20 +0200

    improved game move test with common setup : blue

--- KataFindTheCookie.NUnit/When_Only_1_Move_Is_Defined.cs
+++ KataFindTheCookie.NUnit/When_Only_1_Move_Is_Defined.cs
@@ -7,25 +7,32 @@ namespace KataFindTheCookie.NUnit
 	[TestFixture]
 	public class When_Only_1_Move_Is_Defined 
 	{
+		Definition _definition;
+		CookieTeller _teller;
+		FindTheCookie _game;
+
+		[SetUp]
+		public void Establish_Only_1_Move_Is_Defined()
+		{
+			_definition = new Definition("1 1");
+			_teller = new CookieTeller(1);
+			_game = new FindTheCookie(
+				_definition, 
+				_teller);
+		}
+
 		[Test]
 		public void Should_Joey_Only_Move_Once() 
 		{
-			FindTheCookie game = new FindTheCookie(
-				new Definition("1 1"), 
-				new CookieTeller(1));
-			string[] hints = game.Run();
+			string[] hints = _game.Run();
 			Assert.AreEqual(1, hints.Length);
 		}
 
 		[Test]
 		public void Should_Return_CookieTeller_Message() 
 		{
-			var teller = new CookieTeller(1);
-			FindTheCookie game = new FindTheCookie(
-				new Definition("1 1"), 
-				teller);
-			string[] hints = game.Run();
-			Assert.AreEqual(teller.AreWeThereYet(0, 1), hints[0]);
+			string[] hints = _game.Run();
+			Assert.AreEqual(_teller.AreWeThereYet(0, 1), hints[0]);
 		}
 	}
 }

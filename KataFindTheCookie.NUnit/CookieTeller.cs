52148fa8a9e4a646f3508d364d2212b25cda16e6
Ilker Cetinkaya <ilker@ilker.de>
2013-07-15 22:17:01 +0200

    c1 s0 d2 => same : green

--- KataFindTheCookie.NUnit/CookieTeller.cs
+++ KataFindTheCookie.NUnit/CookieTeller.cs
@@ -5,8 +5,13 @@ namespace KataFindTheCookie.NUnit
 {
 	public class CookieTeller
 	{
+		static int cookie = 1;
+
 		public static string AreWeThereYet (int from, int to)
 		{
+			if (to != cookie)
+				return String.Format ("Moving from 0 to {0}: same.", to);
+
 			return "Moving from 0 to 1: found it!";
 		}
 	}

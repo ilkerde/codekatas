a30975964b3ad39505e13f5697897ce12b21afa5
Ilker Cetinkaya <ilker@ilker.de>
2013-07-16 18:31:54 +0200

    n=5 => x=1,y=4 : red

--- KataBoredomBuster.NUnit/GillianTests.cs
+++ KataBoredomBuster.NUnit/GillianTests.cs
@@ -30,6 +30,15 @@ namespace Kata {
     }
   }
 
+  [TestFixture]
+  public class When_number_is_not_divisible_by_2_or_3 {
+    [Test]
+    public void Then_returns_X_as_1_and_Y_as_remainder() {
+      var split = Gillian.Split(5);
+      split.ShouldSplitTo(1, 4);
+    }
+  }
+
   public static class ShouldExtensions {
     public static void ShouldSplitTo(this SplitPair pair, int x, int y) {
       Assert.AreEqual(x, pair.X);

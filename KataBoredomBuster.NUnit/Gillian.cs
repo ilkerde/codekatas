66de0f28031953d33c7da5ed52b49d1a8fa4e5f8
Ilker Cetinkaya <ilker@ilker.de>
2013-07-16 12:56:08 +0200

    n=9 => x=3,y=6 : green

--- KataBoredomBuster.NUnit/Gillian.cs
+++ KataBoredomBuster.NUnit/Gillian.cs
@@ -3,8 +3,8 @@ namespace Kata {
 
   public class Gillian {
     public static SplitPair Split(int number) {
-      if (number == 3)
-        return new SplitPair { X=1, Y=2 };
+      if (number % 3 == 0)
+        return new SplitPair { X=number/3, Y=2*number/3 };
 
       return new SplitPair { X=number/2, Y=number/2 };
     }

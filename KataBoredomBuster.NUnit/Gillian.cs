af883478756d083c21eebb16ac99dd4ca650f7a1
Ilker Cetinkaya <ilker@ilker.de>
2013-07-16 18:28:53 +0200

    n=1 => stop : green

--- KataBoredomBuster.NUnit/Gillian.cs
+++ KataBoredomBuster.NUnit/Gillian.cs
@@ -3,6 +3,9 @@ namespace Kata {
 
   public class Gillian {
     public static SplitPair Split(int number) {
+      if (number < 2)
+        throw new ArgumentException();
+
       if (number % 3 == 0)
         return new SplitPair { X=number/3, Y=2*number/3 };
 

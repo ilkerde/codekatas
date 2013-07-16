5e3b502d6b6b2d6cd64c444a8bae28651679fd96
Ilker Cetinkaya <ilker@ilker.de>
2013-07-16 18:46:22 +0200

    n=12 => x=6,y=6 : green

--- KataBoredomBuster.NUnit/Gillian.cs
+++ KataBoredomBuster.NUnit/Gillian.cs
@@ -6,11 +6,11 @@ namespace Kata {
       if (number < 2)
         throw new ArgumentException();
 
-      if (number % 3 == 0)
-        return new SplitPair { X=number/3, Y=2*number/3 };
-
       if (number % 2 == 0)
         return new SplitPair { X=number/2, Y=number/2 };
+
+      if (number % 3 == 0)
+        return new SplitPair { X=number/3, Y=2*number/3 };
 
       return new SplitPair { X=1, Y=number-1 };
     }

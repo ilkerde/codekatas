da9e60fbe3d7bdab01e44441dfc8d0230cb38d9c
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 13:12:31 +0200

    reorganized tests in separate files :  blue

---
+++ KataGameOfLife.Vse12/When_Cell_Is_Free_And_Has_No_Alive_Neighbors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataGameOfLife.Vse12
+{
+    [TestClass]
+    public class When_Cell_Is_Free_And_Has_No_Alive_Neighbors
+    {
+        [TestMethod]
+        public void Then_Cell_Will_Stay_Free()
+        {
+            Cell cell = new Cell
+            {
+                IsAlive = false,
+                NumberOfLivingNeighbors = 0
+            };
+
+            cell.NextGeneration();
+
+            Assert.IsFalse(cell.IsAlive);
+        }
+    }
+}

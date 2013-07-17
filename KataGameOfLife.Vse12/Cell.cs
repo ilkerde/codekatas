e9da1d6f7a848a3d663ea4293f0f104c32c93867
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 12:47:14 +0200

    Another GameOfLife. This time exploring the standards of Visual Studio Express 2012 - after a long time. 3 alive neighbors of free cell => cell alive : red

---
+++ KataGameOfLife.Vse12/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KataGameOfLife.Vse12
+{
+    public class Cell
+    {
+        public bool IsAlive { get; set; }
+        public int NumberOfLivingNeighbors { get; set; }
+
+        public void NextGeneration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KataGameOfLife.Vse12/When_Cell_Is_Free_And_Has_3_Alive_Neighbors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataGameOfLife.Vse12
+{
+    [TestClass]
+    public class When_Cell_Is_Free_And_Has_3_Alive_Neighbors
+    {
+        [TestMethod]
+        public void Then_Cell_Will_Be_Alive_In_Next_Generation()
+        {
+            Cell cell = new Cell { 
+                IsAlive = false, 
+                NumberOfLivingNeighbors = 3
+            };
+
+            cell.NextGeneration();
+
+            Assert.IsTrue(cell.IsAlive);
+        }
+    }
+}

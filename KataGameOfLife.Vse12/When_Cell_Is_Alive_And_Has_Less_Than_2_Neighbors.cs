357758c233836e72597409d3c1144df9754f4b2e
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 13:14:54 +0200

    cell alive, less than 2 alive neighbors => free : red

---
+++ KataGameOfLife.Vse12/When_Cell_Is_Alive_And_Has_Less_Than_2_Neighbors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataGameOfLife.Vse12
+{
+    [TestClass]
+    public class When_Cell_Is_Alive_And_Has_Less_Than_2_Neighbors
+    {
+        [TestMethod]
+        public void Then_Cell_Will_Be_Freed()
+        {
+            Cell cell = new Cell
+            {
+                IsAlive = true,
+                NumberOfLivingNeighbors = 1
+            };
+
+            cell.NextGeneration();
+
+            Assert.IsFalse(cell.IsAlive);
+        }
+    }
+}

55ca556dbbaf34dbd0728c9d05651770e53205d0
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 18:46:01 +0200

    world next => co != cn (x+1) : red

---
+++ KataGameOfLife.Vse12/WorldTests/When_World_Is_2x2_Big_And_Moves_To_Next_Generation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataGameOfLife.Vse12
+{
+    [TestClass]
+    public class When_World_Is_2x2_Big_And_Moves_To_Next_Generation
+    {
+        [TestMethod]
+        public void Then_Cell_On_2x1_Is_Different_From_Old_On_Same_Position()
+        {
+            World world = new World(1, 1);
+            Cell oldCell = world.GetCell(2, 1);
+            
+            world.NextGeneration();
+            Cell newCell = world.GetCell(2, 1);
+
+            Assert.AreNotEqual(oldCell, newCell);
+        }
+    }
+}

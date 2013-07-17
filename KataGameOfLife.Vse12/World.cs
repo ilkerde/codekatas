d4da498ac06eb032029c80854ee055cb3d562d27
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 16:22:30 +0200

    in 2x1 world cells are different : green

--- KataGameOfLife.Vse12/World.cs
+++ KataGameOfLife.Vse12/World.cs
@@ -9,14 +9,13 @@ namespace KataGameOfLife.Vse12
 
         public World(int x, int y)
         {
-            _cells = (from i in Enumerable.Range(1, y)
+            _cells = (from i in Enumerable.Range(1, x*y)
                 select new Cell()).ToArray();
-
         }
 
         public Cell GetCell(int x, int y)
         {
-            return _cells[y-1];
+            return _cells[(x-1)*y+(y-1)];
         }
 
         public void SetCellAlive(int x, int y)

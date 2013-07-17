5b4f9abbc98a97fd10d1927c23872d7226c1a7b2
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 16:23:35 +0200

    in 2x1 world all alive cells => cell(2,1) alive : red

---
+++ KataGameOfLife.Vse12/WorldTests/When_World_Of_2x1_Is_Initialized_With_Alive_Cells.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataGameOfLife.Vse12
+{
+    [TestClass]
+    public class When_World_Of_2x1_Is_Initialized_With_Alive_Cells
+    {
+        [TestMethod]
+        public void Then_Cell_At_Position_2x1_Is_Alive()
+        {
+            World world = new World(2, 1);
+            world.SetCellAlive(2, 1);
+            Assert.IsTrue(world.GetCell(2, 1).IsAlive);
+        }
+    }
+}

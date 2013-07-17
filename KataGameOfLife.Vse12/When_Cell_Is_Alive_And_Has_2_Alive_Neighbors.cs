41e79d33ea12c0e463a5c2c958445d138e58f949
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 13:19:49 +0200

    cell alive + 2 alive neighbors => alive : red

---
+++ KataGameOfLife.Vse12/When_Cell_Is_Alive_And_Has_2_Alive_Neighbors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataGameOfLife.Vse12
+{
+    [TestClass]
+    public class When_Cell_Is_Alive_And_Has_2_Alive_Neighbors
+    {
+        [TestMethod]
+        public void Then_Cell_Will_Stay_Alive()
+        {
+            Cell cell = new Cell
+            {
+                IsAlive = true,
+                NumberOfLivingNeighbors = 2
+            };
+
+            cell.NextGeneration();
+
+            Assert.IsTrue(cell.IsAlive);
+        }
+    }
+}

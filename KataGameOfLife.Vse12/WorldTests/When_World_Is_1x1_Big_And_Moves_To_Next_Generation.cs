5785512faf4df4c57014e0076cd9df2d0bc2813d
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 18:44:18 +0200

    renamed test for clarity : blue

--- KataGameOfLife.Vse12/WorldTests/When_World_Is_1x1_Big_And_Moves_To_Next_Generation.cs
+++ KataGameOfLife.Vse12/WorldTests/When_World_Is_1x1_Big_And_Moves_To_Next_Generation.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace KataGameOfLife.Vse12
 {
     [TestClass]
-    public class When_World_Is_Moving_To_Next_Generation
+    public class When_World_Is_1x1_Big_And_Moves_To_Next_Generation
     {
         [TestMethod]
         public void Then_New_Cell_On_Same_Position_Differs_From_Old_Cell()

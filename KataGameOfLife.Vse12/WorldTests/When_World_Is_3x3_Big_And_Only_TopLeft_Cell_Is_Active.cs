e186d2cf903692f1b2f5e25aa1baa8fabc1dd5ab
Ilker Cetinkaya <ilker@ilker.de>
2013-07-17 16:39:33 +0200

    w(3,3), a(1,1), c(3,3).n = 0 : red

--- KataGameOfLife.Vse12/WorldTests/When_World_Is_3x3_Big_And_Only_TopLeft_Cell_Is_Active.cs
+++ KataGameOfLife.Vse12/WorldTests/When_World_Is_3x3_Big_And_Only_TopLeft_Cell_Is_Active.cs
@@ -15,5 +15,15 @@ namespace KataGameOfLife.Vse12
             Cell cell = world.GetCell(2, 2);
             Assert.AreEqual(1, cell.NumberOfLivingNeighbors);
         }
+
+        [TestMethod]
+        public void Then_Cell_In_BottomRight_Has_No_Active_Neighbor()
+        {
+            World world = new World(3, 3);
+            world.SetCellAlive(1, 1);
+
+            Cell cell = world.GetCell(3, 3);
+            Assert.AreEqual(0, cell.NumberOfLivingNeighbors);
+        }
     }
 }

3db8b9cd7e438549c08c4e2ea16f52cdc1a03a26
Ilker Cetinkaya <ilker@ilker.de>
2013-07-18 15:57:08 +0200

    1 => 1 : red

--- KataStringCalculator.XS/StringCalculatorTests.cs
+++ KataStringCalculator.XS/StringCalculatorTests.cs
@@ -12,5 +12,12 @@ namespace KataStringCalculator.XS
 			StringCalculator.Add ("")
 				.ShouldEqual (0);
 		}
+
+		[Test]
+		public void When_1_Is_Given_Should_Sum_To_1 ()
+		{
+			StringCalculator.Add ("1")
+				.ShouldEqual (1);
+		}
 	}
 }

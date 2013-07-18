3bd152b576d3dab390d51ae532644a99fc825a1e
Ilker Cetinkaya <ilker@ilker.de>
2013-07-18 16:06:04 +0200

    2,2 => 4 : green

--- KataStringCalculator.XS/StringCalculator.cs
+++ KataStringCalculator.XS/StringCalculator.cs
@@ -1,16 +1,22 @@
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace KataStringCalculator.XS
 {
 	public class StringCalculator
 	{
-		public static int Add (string numbers)
+		public static int Add (string input)
 		{
-			if (string.IsNullOrEmpty (numbers))
+			if (string.IsNullOrEmpty (input))
 				return 0;
 
-			return Convert.ToInt32 (numbers);
+			var numbers = input.Split (
+				new[] { "," }, 
+				StringSplitOptions.RemoveEmptyEntries
+			);
+
+			return (from number in numbers select Convert.ToInt32 (number)).Sum ();
 		}
 	}
 }

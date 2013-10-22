e21b9b639a92d616b74510fd48637f1c7f6ddd62
Ilker Cetinkaya <ilker@ilker.de>
2013-10-22 23:57:06 +0200

    no multiples of 3 or 5 -> n : green

--- KataFizzBuzz.HSpec/FizzBuzz.hs
+++ KataFizzBuzz.HSpec/FizzBuzz.hs
@@ -2,4 +2,5 @@ module FizzBuzz (fizzbuzz)
 where
   fizzbuzz n | n `mod` 15 == 0 = "FIZZBUZZ"
              | n `mod` 5 == 0 = "BUZZ"
-             | otherwise = "FIZZ"
+             | n `mod` 3 == 0 = "FIZZ"
+             | otherwise = show n

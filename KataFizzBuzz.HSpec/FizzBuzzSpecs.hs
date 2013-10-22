fd85ca946f7a3b8151ebae08f9be528f7d44574b
Ilker Cetinkaya <ilker@ilker.de>
2013-10-22 23:53:53 +0200

    multiples of 15 -> fizzbuzz : red

--- KataFizzBuzz.HSpec/FizzBuzzSpecs.hs
+++ KataFizzBuzz.HSpec/FizzBuzzSpecs.hs
@@ -11,3 +11,7 @@ main = hspec $ do
   describe "multiples of 5" $ do
     it "returns BUZZ" $ do
       fizzbuzz 10 `shouldBe` "BUZZ"
+
+  describe "multiples of 3 and 5" $ do
+    it "returns FIZZBUZZ" $ do
+      fizzbuzz 30 `shouldBe` "FIZZBUZZ"

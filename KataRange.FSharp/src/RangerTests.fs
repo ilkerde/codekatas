390708ff6be9e943bab47e32d0872ff4f27f7e2b
Ilker Cetinkaya <ilker@ilker.de>
2014-02-02 11:40:58 +0100

    4 not in range of 1..3 : green

--- KataRange.FSharp/src/RangerTests.fs
+++ KataRange.FSharp/src/RangerTests.fs
@@ -11,3 +11,7 @@ let ``when range starts with 1 and ends with 3`` () =
 let ``2 is in range of 1..3`` () =
     Assert.True ([1..3] |> has 2)
 
+[<Fact>]
+let ``4 is not in range of 1..3`` () =
+    Assert.False ([1..3] |> has 4)
+

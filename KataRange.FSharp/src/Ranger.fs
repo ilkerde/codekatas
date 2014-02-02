c3b660bbff8b06a9d176400cc04f1f8e52821bfe
Ilker Cetinkaya <ilker@ilker.de>
2014-02-02 11:40:13 +0100

    2 in range of 1..3 : green

--- KataRange.FSharp/src/Ranger.fs
+++ KataRange.FSharp/src/Ranger.fs
@@ -3,3 +3,4 @@ module Ranger
 open System
 
 let rng lo hi = [lo..hi]
+let has i s = s |> List.exists (fun x -> x = i)
--- KataRange.FSharp/src/RangerTests.fs
+++ KataRange.FSharp/src/RangerTests.fs
@@ -7,3 +7,7 @@ open Xunit
 let ``when range starts with 1 and ends with 3`` () =
     Assert.Equal (3, rng 1 3 |> List.length)
 
+[<Fact>]
+let ``2 is in range of 1..3`` () =
+    Assert.True ([1..3] |> has 2)
+

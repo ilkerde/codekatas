2fe38f57dd346f4c2f704ca82d7a16d18dd46b32
Ilker Cetinkaya <ilker@ilker.de>
2014-02-16 10:34:10 +0100

    1..4 intersect 2..5 = 2..4 : green

--- KataRange.FSharp/src/Ranger.fs
+++ KataRange.FSharp/src/Ranger.fs
@@ -2,5 +2,13 @@ module Ranger
 
 open System
 
-let rng lo hi = [lo..hi]
-let has i s = s |> List.exists (fun x -> x = i)
+let rng lo hi = 
+    [lo..hi]
+
+let has i s = 
+    s |> List.exists (fun x -> x = i)
+
+let intersect s1 s2 = 
+    s1 |> Set.ofList
+       |> Set.intersect (Set.ofList s2)
+       |> Set.toList

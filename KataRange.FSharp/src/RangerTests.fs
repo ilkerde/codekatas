54dc8962219c66f219e485b02776a13d3ceb15b0
Ilker Cetinkaya <ilker@ilker.de>
2014-02-16 10:32:31 +0100

    1..4 intersect 2..5 = 2..4 : red

--- KataRange.FSharp/src/RangerTests.fs
+++ KataRange.FSharp/src/RangerTests.fs
@@ -15,3 +15,10 @@ let ``2 is in range of 1..3`` () =
 let ``4 is not in range of 1..3`` () =
     Assert.False ([1..3] |> has 4)
 
+[<Fact>]
+let ``1..3 intersect with emtpy is empty`` () =
+    Assert.Equal (0, [1..3] |> intersect [] |> List.length)
+
+[<Fact>]
+let ``1..4 intersect with 2..5 is 2..4`` () =
+    Assert.Equal<int> ([2..4], [1..4] |> intersect [2..5])

133fe49d8a40568f8577daf27108a86d5feabdb5
Ilker Cetinkaya <ilker@ilker.de>
2014-02-16 10:41:58 +0100

    touchdown. range kata in f# is great. it shows once more how concise and clean f# is.

--- KataRange.FSharp/src/Ranger.fs
+++ KataRange.FSharp/src/Ranger.fs
@@ -9,6 +9,4 @@ let has i s =
     s |> List.exists (fun x -> x = i)
 
 let intersect s1 s2 = 
-    s1 |> Set.ofList
-       |> Set.intersect (Set.ofList s2)
-       |> Set.toList
+    Set.intersect (set s1) (set s2) |> Set.toList
